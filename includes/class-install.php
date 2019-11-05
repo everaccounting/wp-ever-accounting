491228228cc80bdeb02ec6c3e50b691c8bb04828
MD Sultan Nasir Uddin <manikdrmc@gmail.com>
2019-11-05 15:01:06 +0600

    initial

---
+++ includes/class-install.php
@@ -0,0 +1,180 @@
+<?php
+defined('ABSPATH') || exit();
+
+class EAccounting_Install{
+	/**
+	 * Everything need to be done
+	 *
+	 * @since 1.0.0
+	 */
+	public static function install(){
+		self::create_tables();
+	}
+
+	/**
+	 * Delete all data
+	 *
+	 * @since 1.0.0
+	 */
+	public static function uninstall(){
+
+	}
+
+	/**
+	 * Creat tables
+	 * @since 1.0.0
+	 */
+	public static function create_tables() {
+		global $wpdb;
+		$wpdb->hide_errors();
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		$tables = [
+			"CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ea_accounts(
+            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+		    `name` varchar(191) NOT NULL COMMENT 'Account Name',
+		    `number` varchar(191) NOT NULL COMMENT 'Account Number',
+		    `currency_code` varchar(10) NOT NULL,
+		    `opening_balance` double(15,4) NOT NULL DEFAULT '0.0000',
+		    `bank_name` varchar(191) DEFAULT NULL,
+		    `bank_phone` varchar(20) DEFAULT NULL,
+		    `bank_address` varchar(191) DEFAULT NULL,
+		    `enabled` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Account enable or disable',
+		    `created_at` DATETIME NULL DEFAULT NULL COMMENT 'Create Date',
+		    `updated_at` DATETIME NULL DEFAULT NULL COMMENT 'Update Date',
+		    PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8",
+
+//            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ea_transfers(
+//            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+//            `company_id` int(11) NOT NULL,
+//            `payment_id` int(11) NOT NULL,
+//            `revenue_id` int(11) NOT NULL,
+//		    `created_at` DATETIME NULL DEFAULT NULL COMMENT 'Create Date',
+//		    `updated_at` DATETIME NULL DEFAULT NULL COMMENT 'Update Date',
+//		    PRIMARY KEY (`id`),
+//		    KEY `company_id` (`company_id`)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8",
+//            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ea_invoices(
+//            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+//            `invoice_number` varchar(191) NOT NULL,
+//            `order_number` varchar(191) DEFAULT NULL,
+//            `invoice_status_code` varchar(191) NOT NULL,
+//            `invoiced_at` datetime NOT NULL,
+//            `due_at` datetime NOT NULL,
+//            `amount` double(15,4) NOT NULL,
+//            `currency_code` varchar(191) NOT NULL,
+//            `currency_rate` double(15,8) NOT NULL,
+//            `customer_id` int(11) NOT NULL,
+//            `customer_name` varchar(191) NOT NULL,
+//            `customer_email` varchar(191) DEFAULT NULL,
+//            `customer_tax_number` varchar(191) DEFAULT NULL,
+//            `customer_phone` varchar(191) DEFAULT NULL,
+//            `customer_address` text,
+//            `notes` text,
+//            `category_id` int(11) NOT NULL DEFAULT '1',
+//            `parent_id` int(11) NOT NULL DEFAULT '0',
+//		    `created_at` DATETIME NULL DEFAULT NULL COMMENT 'Create Date',
+//		    `updated_at` DATETIME NULL DEFAULT NULL COMMENT 'Update Date',
+//		    PRIMARY KEY (`id`),
+//		    KEY `company_id` (`company_id`)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8",
+//            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ea_invoice_totals(
+//            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+//            `company_id` int(11) NOT NULL,
+//            `invoice_id` int(11) NOT NULL,
+//            `code` varchar(191) DEFAULT NULL,
+//            `name` varchar(191) NOT NULL,
+//            `amount` double(15,4) NOT NULL,
+//            `sort_order` int(11) NOT NULL,
+//		    `created_at` DATETIME NULL DEFAULT NULL COMMENT 'Create Date',
+//		    `updated_at` DATETIME NULL DEFAULT NULL COMMENT 'Update Date',
+//		    PRIMARY KEY (`id`),
+//		    KEY `company_id` (`company_id`)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8",
+//            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ea_invoice_payments(
+//            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+//            `company_id` int(11) NOT NULL,
+//            `invoice_id` int(11) NOT NULL,
+//            `account_id` int(11) NOT NULL,
+//            `amount` double(15,4) NOT NULL,
+//            `currency_code` varchar(191) NOT NULL,
+//            `currency_rate` double(15,8) NOT NULL,
+//            `description` text,
+//            `payment_method` varchar(191) NOT NULL,
+//            `reference` varchar(191) DEFAULT NULL,
+//            `paid_at` datetime NOT NULL,
+//            `reconciled` tinyint(1) NOT NULL DEFAULT '0',
+//		    `created_at` DATETIME NULL DEFAULT NULL COMMENT 'Create Date',
+//		    `updated_at` DATETIME NULL DEFAULT NULL COMMENT 'Update Date',
+//		    PRIMARY KEY (`id`),
+//		    KEY `company_id` (`company_id`)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8",
+//            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ea_invoice_payments(
+//            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+//            `company_id` int(11) NOT NULL,
+//            `invoice_id` int(11) NOT NULL,
+//            `item_id` int(11) DEFAULT NULL,
+//            `name` varchar(191) NOT NULL,
+//            `sku` varchar(191) DEFAULT NULL,
+//            `quantity` double(7,2) NOT NULL,
+//            `price` double(15,4) NOT NULL,
+//            `total` double(15,4) NOT NULL,
+//            `tax` double(15,4) NOT NULL DEFAULT '0.0000',
+//		    `created_at` DATETIME NULL DEFAULT NULL COMMENT 'Create Date',
+//		    `updated_at` DATETIME NULL DEFAULT NULL COMMENT 'Update Date',
+//		    PRIMARY KEY (`id`),
+//		    KEY `company_id` (`company_id`)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8",
+//            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ea_invoice_item_taxes(
+//            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+//            `company_id` int(11) NOT NULL,
+//            `invoice_id` int(11) NOT NULL,
+//            `invoice_item_id` int(11) NOT NULL,
+//            `tax_id` int(11) NOT NULL,
+//            `name` varchar(191) NOT NULL,
+//            `amount` double(15,4) NOT NULL DEFAULT '0.0000',
+//		    `created_at` DATETIME NULL DEFAULT NULL COMMENT 'Create Date',
+//		    `updated_at` DATETIME NULL DEFAULT NULL COMMENT 'Update Date',
+//		    PRIMARY KEY (`id`),
+//		    KEY `company_id` (`company_id`)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8",
+//            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ea_invoice_item_taxes(
+//            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+//            `company_id` int(11) NOT NULL,
+//            `invoice_id` int(11) NOT NULL,
+//            `status_code` varchar(191) NOT NULL,
+//            `notify` tinyint(1) NOT NULL,
+//            `description` text,
+//		    `created_at` DATETIME NULL DEFAULT NULL COMMENT 'Create Date',
+//		    `updated_at` DATETIME NULL DEFAULT NULL COMMENT 'Update Date',
+//		    PRIMARY KEY (`id`),
+//		    KEY `company_id` (`company_id`)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8",
+//            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ea_payments(
+//            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+//            `company_id` int(11) NOT NULL,
+//            `account_id` int(11) NOT NULL,
+//            `paid_at` datetime NOT NULL,
+//            `amount` double(15,4) NOT NULL,
+//            `currency_code` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+//            `currency_rate` double(15,8) NOT NULL,
+//            `vendor_id` int(11) DEFAULT NULL,
+//            `description` text COLLATE utf8mb4_unicode_ci,
+//            `category_id` int(11) NOT NULL,
+//            `payment_method` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+//            `reference` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+//            `parent_id` int(11) NOT NULL DEFAULT '0',
+//            `reconciled` tinyint(1) NOT NULL DEFAULT '0',
+//		    `created_at` DATETIME NULL DEFAULT NULL COMMENT 'Create Date',
+//		    `updated_at` DATETIME NULL DEFAULT NULL COMMENT 'Update Date',
+//		    PRIMARY KEY (`id`),
+//		    KEY `company_id` (`company_id`)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8",
+		];
+
+
+		foreach ( $tables as $table ) {
+			dbDelta( $table );
+		}
+	}
+}
---
+++ wp-ever-accounting.php
@@ -0,0 +1,237 @@
+<?php
+/**
+ * Plugin Name: WP Ever Accounting
+ * Plugin URI: https://pluginever.com/plugins/wp-ever-crm
+ * Description: Best WordPress CRM plugin
+ * Version: 1.0.0
+ * Author: pluginever
+ * Author URI: https://pluginever.com/
+ * Requires at least: 4.7.0
+ * Tested up to: 5.1
+ * Text Domain: wp-ever-accounting
+ * Domain Path: /languages/
+ * License: GPL2+
+ *
+ * @package wp-ever-accounting
+ */
+
+defined( 'ABSPATH' ) || exit();
+
+final class EverAccounting {
+    /**
+     * EverAccounting version.
+     *
+     * @var string
+     */
+    public $version = '1.0.0';
+
+    /**
+     * The single instance of the class.
+     *
+     * @var EverAccounting
+     * @since 1.0.0
+     */
+    protected static $_instance = null;
+
+    /**
+     * Main EverAccounting Instance.
+     *
+     * Ensures only one instance of EverAccounting is loaded or can be loaded.
+     *
+     * @return EverAccounting - Main instance.
+     * @since 1.0.0
+     * @static
+     */
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+
+    /**
+     * Cloning is forbidden.
+     *
+     * @return void
+     * @since 1.0.0
+     */
+    public function __clone() {
+        _doing_it_wrong( __FUNCTION__, __( 'Cloning is forbidden.', 'wp-ever-accounting' ), '1.0.0' );
+    }
+
+    /**
+     * Universalizing instances of this class is forbidden.
+     *
+     * @return void
+     * @since 1.0.0
+     */
+    public function __wakeup() {
+        _doing_it_wrong( __FUNCTION__, __( 'Unserializing instances of this class is forbidden.', 'wp-ever-accounting' ), '1.0.0' );
+    }
+
+    /**
+     * WooCommerce Constructor.
+     */
+    public function __construct() {
+        $this->define_constants();
+        $this->define_tables();
+        $this->includes();
+        $this->init_hooks();
+    }
+
+
+    /**
+     * define all required constants
+     *
+     * since 1.0.0
+     *
+     * @return void
+     */
+    public function define_constants() {
+        define( 'EVER_ACCOUNTING_VERSION', $this->version );
+        define( 'EVER_ACCOUNTING_DB_VERSION', '20181123' );
+        define( 'EVER_ACCOUNTING_PLUGIN_FILE', __FILE__ );
+        define( 'EVER_ACCOUNTING_ABSPATH', dirname( EVER_ACCOUNTING_PLUGIN_FILE ) );
+        define( 'EVER_ACCOUNTING_URL', plugins_url( '', EVER_ACCOUNTING_PLUGIN_FILE ) );
+        define( 'EVER_ACCOUNTING_ASSETS_URL', EVER_ACCOUNTING_URL . '/assets' );
+        define( 'EVER_ACCOUNTING_TEMPLATES_DIR', EVER_ACCOUNTING_ABSPATH . '/templates' );
+        define( 'EVER_ACCOUNTING_CACHE_KEY', 'eacc' );
+        define( 'EVER_ACCOUNTING_DB_PREFIX', 'eacc_' );
+    }
+
+    /**
+     * Register custom tables within $wpdb object.
+     */
+    private function define_tables() {
+        global $wpdb;
+        $tables = array(
+            'ea_accounts',
+        );
+        foreach ( $tables as $table ) {
+            $wpdb->$table   = $wpdb->prefix . $table;
+            $wpdb->tables[] = $table;
+        }
+    }
+
+    /**
+     * Include all required files
+     *
+     * since 1.0.0
+     *
+     * @return void
+     */
+    public function includes() {
+        require_once ( EVER_ACCOUNTING_ABSPATH . '/includes/class-install.php' );
+    }
+
+    /**
+     * Hook into actions and filters.
+     *
+     * @since 1.0.0
+     */
+    private function init_hooks() {
+        register_activation_hook( EVER_ACCOUNTING_PLUGIN_FILE, array( 'EAccounting_Install', 'install' ) );
+
+        add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded' ), - 1 );
+        add_action( 'init', array( $this, 'init' ), 0 );
+        add_action( 'init', array( $this, 'localization_setup' ) );
+        add_action( 'activated_plugin', array( $this, 'activated_plugin' ) );
+        add_action( 'deactivated_plugin', array( $this, 'deactivated_plugin' ) );
+    }
+
+    /**
+     * When WP has loaded all plugins, trigger the `ever_accounting_loaded` hook.
+     *
+     * This ensures `ever_accounting_loaded` is called only after all other plugins
+     * are loaded, to avoid issues caused by plugin directory naming changing
+     *
+     * @since 1.0.0
+     */
+    public function on_plugins_loaded() {
+        do_action( 'ever_accounting_loaded' );
+    }
+
+    /**
+     * Init EverAccounting when WordPress Initialises.
+     */
+    public function init() {
+        // Before init action.
+        do_action( 'before_ever_accounting_init' );
+
+        // Init action.
+        do_action( 'ever_accounting_init' );
+    }
+
+    /**
+     * Initialize plugin for localization
+     *
+     * @return void
+     * @since 1.0.0
+     *
+     */
+    public function localization_setup() {
+        load_plugin_textdomain( 'wp-ever-accounting', false, dirname( plugin_basename( __FILE__ ) ) . '/i18n/languages/' );
+    }
+
+    /**
+     * Ran when any plugin is activated.
+     *
+     * @since 1.0.0
+     * @param string $filename The filename of the activated plugin.
+     */
+    public function activated_plugin( $filename ) {
+
+    }
+
+    /**
+     * Ran when any plugin is deactivated.
+     *
+     * @since 1.0.0
+     * @param string $filename The filename of the deactivated plugin.
+     */
+    public function deactivated_plugin( $filename ) {
+
+    }
+
+    /**
+     * Get the plugin url.
+     *
+     * @return string
+     */
+    public function plugin_url() {
+        return untrailingslashit( plugins_url( '/', EVER_ACCOUNTING_PLUGIN_FILE ) );
+    }
+
+    /**
+     * Get the plugin path.
+     *
+     * @return string
+     */
+    public function plugin_path() {
+        return untrailingslashit( plugin_dir_path( EVER_ACCOUNTING_PLUGIN_FILE ) );
+    }
+
+    /**
+     * Get the template path.
+     *
+     * @return string
+     */
+    public function template_path() {
+        return apply_filters( 'ever_accounting_template_path', 'ever-accounting/' );
+    }
+}
+
+
+/**
+ * Returns the main instance of Plugin.
+ *
+ * @return EverAccounting
+ * @since  1.0.0
+ */
+function ever_accounting() {
+    return EverAccounting::instance();
+}
+
+ever_accounting();
