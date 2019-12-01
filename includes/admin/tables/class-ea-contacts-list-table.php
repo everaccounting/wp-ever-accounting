1009cef3b523d8ebdd7da74e16d216bfe3a9622d
Nazrul Islam Nayan <nazrulislamnayan7@gmail.com>
2019-12-01 16:21:02 +0600

    contact & category action preview

--- includes/admin/tables/class-ea-contacts-list-table.php
+++ includes/admin/tables/class-ea-contacts-list-table.php
@@ -105,6 +105,84 @@ class EAccounting_Contacts_List_Table extends WP_List_Table {
 	}
 
 	/**
+	 * Message to be displayed when there are no items
+	 *
+	 * @since 1.0.0
+	 */
+	function no_items() {
+		echo sprintf( __( 'No %s found.', 'wp-ever-accounting' ), $this->_args['plural'] );
+	}
+
+	/**
+	 * Show the search field
+	 *
+	 * @param string $text Label for the search box
+	 * @param string $input_id ID of the search box
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 *
+	 */
+	public function search_box( $text, $input_id ) {
+		if ( empty( $_REQUEST['s'] ) && ! $this->has_items() ) {
+			return;
+		}
+
+		$input_id = $input_id . '-search-input';
+
+		if ( ! empty( $_REQUEST['orderby'] ) ) {
+			echo '<input type="hidden" name="orderby" value="' . esc_attr( $_REQUEST['orderby'] ) . '" />';
+		}
+		if ( ! empty( $_REQUEST['order'] ) ) {
+			echo '<input type="hidden" name="order" value="' . esc_attr( $_REQUEST['order'] ) . '" />';
+		}
+		?>
+		<p class="search-box">
+			<label class="screen-reader-text" for="<?php echo $input_id ?>"><?php echo $text; ?>:</label>
+			<input type="search" id="<?php echo $input_id ?>" name="s" value="<?php _admin_search_query(); ?>"/>
+			<?php submit_button( $text, 'button', false, false, array( 'ID' => 'search-submit' ) ); ?>
+		</p>
+		<?php
+	}
+
+	/**
+	 * Retrieve the view types
+	 *
+	 * @return array $views All the views available
+	 * @since 1.0.0
+	 */
+	public function get_views() {
+		$current        = isset( $_GET['status'] ) ? sanitize_key( $_GET['status'] ) : '';
+		$total_count    = '&nbsp;<span class="count">(' . $this->total_count . ')</span>';
+		$active_count   = '&nbsp;<span class="count">(' . $this->active_count . ')</span>';
+		$inactive_count = '&nbsp;<span class="count">(' . $this->inactive_count . ')</span>';
+
+		$views = array(
+			'all'      => sprintf( '<a href="%s"%s>%s</a>', remove_query_arg( 'status', $this->base_url ), $current === 'all' || $current == '' ? ' class="current"' : '', __( 'All', 'wp-ever-accounting' ) . $total_count ),
+			'active'   => sprintf( '<a href="%s"%s>%s</a>', add_query_arg( 'status', 'active', $this->base_url ), $current === 'active' ? ' class="current"' : '', __( 'Active', 'wp-ever-accounting' ) . $active_count ),
+			'inactive' => sprintf( '<a href="%s"%s>%s</a>', add_query_arg( 'status', 'inactive', $this->base_url ), $current === 'inactive' ? ' class="current"' : '', __( 'Inactive', 'wp-ever-accounting' ) . $inactive_count ),
+		);
+
+		return $views;
+	}
+
+	/**
+	 * Retrieve the bulk actions
+	 *
+	 * @return array $actions Array of the bulk actions
+	 * @since 1.0.0
+	 */
+	public function get_bulk_actions() {
+		$actions = array(
+			'activate'   => __( 'Activate', 'wp-ever-accounting' ),
+			'deactivate' => __( 'Deactivate', 'wp-ever-accounting' ),
+			'delete'     => __( 'Delete', 'wp-ever-accounting' ),
+		);
+
+		return $actions;
+	}
+
+	/**
 	 * Retrieve the table columns
 	 *
 	 * @return array $columns Array of all the list table columns
