62593519a6abb7dc5d353cd7096203408581be54
sudipto_me <sudipto004.ruet@gmail.com>
2020-08-27 18:24:37 +0600

    transfer add, update delete test added

---
+++ tests/unit-tests/transfer/class-ea-test-transfer.php
@@ -0,0 +1,72 @@
+<?php
+
+use EverAccounting\Transfer;
+
+/**
+ * Class EAccounting_Tests_transfer.
+ * @package EAccounting\Tests\Category
+ */
+
+class EAccounting_Tests_transfer extends EAccounting_Unit_Test_Case {
+	public function test_create_transfer(){
+		$from_account_id = EAccounting_Helper_Account::create_account('From Account','001')->get_id();
+		$to_account_id = EAccounting_Helper_Account::create_account('To Account','002')->get_id();
+		$transfer = eaccounting_insert_transfer(array(
+			'from_account_id' => $from_account_id,
+			'to_account_id' => $to_account_id,
+			'date' => '2020-08-26',
+			'amount' => 50,
+			'payment_method' => 'cash',
+			'reference' => 'Test Reference',
+			'description' => "Testing transfer for unit testing"
+		));
+		$this->assertNotFalse( $transfer->exists() );
+
+		$this->assertEquals('2020-08-26',date('Y-m-d', strtotime($transfer->get_date())));
+		$this->assertEquals(50,$transfer->get_amount());
+		$this->assertEquals('cash',$transfer->get_payment_method());
+		$this->assertEquals('Test Reference',$transfer->get_reference());
+		$this->assertEquals('Testing transfer for unit testing',$transfer->get_description());
+
+	}
+
+	public function test_update_transfer(){
+		$from_account_id = EAccounting_Helper_Account::create_account('From Account','001')->get_id();
+		$to_account_id = EAccounting_Helper_Account::create_account('To Account','002')->get_id();
+		$transfer = eaccounting_insert_transfer(array(
+			'from_account_id' => $from_account_id,
+			'to_account_id' => $to_account_id,
+			'date' => '2020-08-26',
+			'amount' => 50,
+			'payment_method' => 'cash',
+		));
+		$transfer_id = $transfer->get_id();
+		$this->assertNotFalse( $transfer->exists() );
+
+		$this->assertEquals('2020-08-26',date('Y-m-d', strtotime($transfer->get_date())));
+		$this->assertEquals(50,$transfer->get_amount());
+		$this->assertEquals('cash',$transfer->get_payment_method());
+
+		$error = eaccounting_insert_transfer(array(
+			'id' => $transfer_id,
+			'from_account_id' => $to_account_id,
+			'to_account_id' => $from_account_id,
+			'date' => '2020-08-24',
+			'amount' => 60,
+			'payment_method' => 'cash',
+		));
+		$this->assertNotWPError( $error );
+
+		$transfer = eaccounting_get_transfer( $transfer_id ); // so we can read fresh copies from the DB
+
+		$this->assertEquals('2020-08-24',date('Y-m-d', strtotime($transfer->get_date())));
+		$this->assertEquals(60,$transfer->get_amount());
+		$this->assertEquals('cash',$transfer->get_payment_method());
+	}
+
+	public function test_delete_transfer() {
+		$transfer = EAccounting_Helper_Transfer::create_transfer();
+		$this->assertNotEquals( 0, $transfer->get_id() );
+		$this->assertNotFalse( eaccounting_delete_transfer( $transfer->get_id() ) );
+	}
+}
