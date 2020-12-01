46202447a1e8811764760d363a6d10e783fcde28
sudipto-me <sudipto004.ruet@gmail.com>
2020-12-01 10:38:11 +0600

    add invoice form updated

--- includes/admin/sales/invoices/edit-invoice.php
+++ includes/admin/sales/invoices/edit-invoice.php
@@ -28,50 +28,54 @@ $back_url = remove_query_arg( array( 'action', 'invoice_id' ) );
 		<form id="ea-category-form" class="ea-ajax-form" method="post">
 			<div class="ea-row">
 				<?php
-				eaccounting_contact_dropdown( array(
-								'wrapper_class' => 'ea-col-6',
-								'label'         => __( 'Customer', 'wp-ever-accounting' ),
-								'name'          => 'contact_id',
-								'id'            => 'customer_id',
-								'placeholder'   => __( 'Select Customer', 'wp-ever-accounting' ),
-								'value'         => $invoice->get_contact_id(),
-								'required'      => true,
-								'type'          => 'customer',
-								'creatable'     => true,
-						)
-				);
-				eaccounting_currency_dropdown(
-						array(
-								'wrapper_class' => 'ea-col-6',
-								'label'         => __( 'Account Currency', 'wp-ever-accounting' ),
-								'name'          => 'currency_code',
-								'value'         => $invoice->get_currency_code(),
-								'required'      => true,
-								'creatable'     => true,
-						)
-				);
+				eaccounting_contact_dropdown(array(
+					'wrapper_class' => 'ea-col-6',
+					'label'         => __( 'Customer', 'wp-ever-accounting' ),
+					'name'          => 'contact_id',
+					'id'            => 'customer_id',
+					'placeholder'   => __( 'Select Customer', 'wp-ever-accounting' ),
+					'value'         => $invoice->get_contact_id(),
+					'required'      => true,
+					'type'          => 'customer',
+					'creatable'     => true,
+				) );
+				eaccounting_currency_dropdown(array(
+					'wrapper_class' => 'ea-col-6',
+					'label'         => __( 'Account Currency', 'wp-ever-accounting' ),
+					'name'          => 'currency_code',
+					'value'         => $invoice->get_currency_code(),
+					'required'      => true,
+					'creatable'     => true,
+				));
 				eaccounting_text_input( array(
-						'wrapper_class' => 'ea-col-6',
-						'label'         => __( 'Invoice Date', 'wp-ever-accounting' ),
-						'name'          => 'invoiced_at',
-						'value'         => $invoice->get_invoiced_at() ? $invoice->get_invoiced_at() : null,
-						'required'      => true,
-						'data_type'     => 'date'
+					'wrapper_class' => 'ea-col-6',
+					'label'         => __( 'Invoice Date', 'wp-ever-accounting' ),
+					'name'          => 'invoiced_at',
+					'value'         => $invoice->get_invoiced_at() ? $invoice->get_invoiced_at() : null,
+					'required'      => true,
+					'data_type'     => 'date'
+				));
+				eaccounting_text_input( array(
+					'wrapper_class' => 'ea-col-6',
+					'label'         => __( 'Due Date', 'wp-ever-accounting' ),
+					'name'          => 'due_at',
+					'value'         => $invoice->get_due_at() ? $invoice->get_due_at() : null,
+					'required'      => true,
+					'data_type'     => 'date'
 				) );
 				eaccounting_text_input( array(
-						'wrapper_class' => 'ea-col-6',
-						'label'         => __( 'Due Date', 'wp-ever-accounting' ),
-						'name'          => 'due_at',
-						'value'         => $invoice->get_due_at() ? $invoice->get_due_at() : null,
-						'required'      => true,
-						'data_type'     => 'date'
+					'wrapper_class' => 'ea-col-6',
+					'label'         => __( 'Invoice Number', 'wp-ever-accounting' ),
+					'name'          => 'invoice_number',
+					'value'         => $invoice->get_invoice_number(),
+					'required'      => true,
 				) );
 				eaccounting_text_input( array(
-						'wrapper_class' => 'ea-col-6',
-						'label'         => __( 'Invoice Number', 'wp-ever-accounting' ),
-						'name'          => 'invoice_number',
-						'value'         => $invoice->get_invoice_number(),
-						'required'      => true,
+					'wrapper_class' => 'ea-col-6',
+					'label'         => __( 'Order Number', 'wp-ever-accounting' ),
+					'name'          => 'order_number',
+					'value'         => $invoice->get_order_number(),
+					'required'      => false,
 				) );
 				?>
 				<div class="ea-form-field ea-col-12 ea-invoice-table-wrap">
@@ -79,54 +83,53 @@ $back_url = remove_query_arg( array( 'action', 'invoice_id' ) );
 					<table cellpadding="0" cellspacing="0" class="ea-invoice-items-table">
 						<thead class="ea-invoice-items-table-head">
 						<tr>
-							<th class="ea-invoice-item"><?php _e( 'Item', 'wp-ever-accounting' ); ?></th>
+							<th class="ea-invoice-item"><?php _e( 'Item Name', 'wp-ever-accounting' ); ?></th>
 							<th class="ea-invoice-item-quantity"><?php _e( 'Quantity', 'wp-ever-accounting' ) ?></th>
 							<th colspan="ea-invoice-item-price"><?php _e( 'Price', 'wp-ever-accounting' ) ?></th>
-							<th colspan="ea-invoice-item-actions"></th>
+							<th colspan="ea-invoice-item-total"><?php _e( 'Total', 'wp-ever-accounting' ) ?></th>
+							<th colspan="ea-invoice-item-actions">&nbsp;</th>
 						</tr>
 						</thead>
 						<tbody class="ea-invoice-items-table-body">
 						<tr class="ea-invoice-items-line-items">
 							<td class="ea-invoice-item"><?php _e( 'Item 1', 'wp-ever-accounting' ); ?></td>
 							<td class="ea-invoice-item-quantity"><?php _e( '1', 'wp-ever-accounting' ) ?></td>
-							<td colspan="ea-invoice-item-price"><?php _e( '$1000.00', 'wp-ever-accounting' ) ?></td>
-							<td colspan="ea-invoice-item-actions">
+							<td class="ea-invoice-item-price"><?php _e( '$1000.00', 'wp-ever-accounting' ) ?></td>
+							<td class="ea-invoice-item-total"><?php _e( '$1000.00', 'wp-ever-accounting' ) ?></td>
+							<td class="ea-invoice-item-actions">
 								<span class="dashicons dashicons-edit-page"></span>
 								<span class="dashicons dashicons-trash"></span>
 							</td>
 						</tr>
+						<tr class="ea-invoice-items-line-items">
+							<td class="ea-invoice-item"><?php _e( 'Item 1', 'wp-ever-accounting' ); ?></td>
+							<td class="ea-invoice-item-quantity"><?php _e( '1', 'wp-ever-accounting' ) ?></td>
+							<td class="ea-invoice-item-price"><?php _e( '$1000.00', 'wp-ever-accounting' ) ?></td>
+							<td class="ea-invoice-item-total"><?php _e( '$1000.00', 'wp-ever-accounting' ) ?></td>
+							<td class="ea-invoice-item-actions">
+								<span class="dashicons dashicons-edit-page"></span>
+								<span class="dashicons dashicons-trash"></span>
+							</td>
+						</tr>
+
 						</tbody>
 					</table>
 					<div class="ea-invoice-items-total">
 						<div class="ea-row">
-							<div class="ea-col-6"></div>
-							<div class="ea-col-6">
-								<table class="ea-invoice-total text-right">
+							<div class="ea-col-12">
+								<table class="ea-invoice-total">
 									<tbody>
-									<tr class="ea-totals-subtotal">
-										<td class="label"><?php _e( 'Items Subtotal:','wp-ever-accounting' ); ?></td>
-										<td width="1%"></td>
-										<td class="value">$1000.00</td>
+									<tr id="subtotal">
+										<td><?php _e( 'Items Subtotal: $1000.00','wp-ever-accounting' ); ?></td>
 									</tr>
-									<tr class="ea-totals-discount">
-										<td class="label"><?php _e( 'Discount:','wp-ever-accounting' ); ?></td>
-										<td width="1%"></td>
-										<td class="value">$0.00</td>
+									<tr id="discount">
+										<td><?php _e( 'Discount: $0.00','wp-ever-accounting' ); ?></td>
 									</tr>
-									<tr class="ea-totals-shipping">
-										<td class="label"><?php _e( 'Shipping:','wp-ever-accounting' ); ?></td>
-										<td width="1%"></td>
-										<td class="value">$0.00</td>
+									<tr id="shipping">
+										<td><?php _e( 'Shipping: $0.00','wp-ever-accounting' ); ?></td>
 									</tr>
-									<tr class="ea-totals-shipping">
-										<td class="label"><?php _e( 'Shipping:','wp-ever-accounting' ); ?></td>
-										<td width="1%"></td>
-										<td class="value">$0.00</td>
-									</tr>
-									<tr class="ea-totals-total">
-										<td class="label"><?php _e( 'Items Total:','wp-ever-accounting' ); ?></td>
-										<td width="1%"></td>
-										<td class="value">$1000.00</td>
+									<tr id="total">
+										<td><?php _e( 'Items Total: $1000.00','wp-ever-accounting' ); ?></td>
 									</tr>
 									</tbody>
 								</table>
@@ -134,11 +137,11 @@ $back_url = remove_query_arg( array( 'action', 'invoice_id' ) );
 						</div>
 						<div class="ea-invoice-items-actions">
 							<div class="ea-row">
-								<div class="ea-col-8 text-left">
+								<div class="ea-col-8">
 									<button type="button" class="button ea-add-invoice-item btn-secondary"><?php _e('Add Invoice Items','wp-ever-accounting');?></button>
 									<button type="button" class="button ea-create-invoice-item btn-secondary"><?php _e('Create Invoice Items','wp-ever-accounting');?></button>
 								</div>
-								<div class="ea-col-4 text-right">
+								<div class="ea-col-4 align-right">
 									<button type="button" class="button ea-invoice-recalculate button-primary"><?php _e('Recalculate Totals','wp-ever-accounting');?></button>
 								</div>
 							</div>
@@ -146,43 +149,45 @@ $back_url = remove_query_arg( array( 'action', 'invoice_id' ) );
 					</div>
 				</div>
 				<?php
-				eaccounting_text_input( array(
-						'wrapper_class' => 'ea-col-6',
-						'label'         => __( 'Order Number', 'wp-ever-accounting' ),
-						'name'          => 'order_number',
-						'value'         => $invoice->get_order_number(),
-						'required'      => false,
+				eaccounting_textarea( array(
+					'wrapper_class' => 'ea-col-6',
+					'label'         => __( 'Notes', 'wp-ever-accounting' ),
+					'name'          => 'note',
+					'value'         => $invoice->get_note(),
+					'required'      => false,
 				) );
 				eaccounting_textarea( array(
-						'wrapper_class' => 'ea-col-6',
-						'label'         => __( 'Notes', 'wp-ever-accounting' ),
-						'name'          => 'note',
-						'value'         => $invoice->get_note(),
-						'required'      => false,
-				) );
-				eaccounting_textarea( array(
-						'wrapper_class' => 'ea-col-6',
-						'label'         => __( 'Footer', 'wp-ever-accounting' ),
-						'name'          => 'footer',
-						'value'         => $invoice->get_footer(),
-						'required'      => false,
+					'wrapper_class' => 'ea-col-6',
+					'label'         => __( 'Footer', 'wp-ever-accounting' ),
+					'name'          => 'footer',
+					'value'         => $invoice->get_footer(),
+					'required'      => false,
 				) );
 				eaccounting_category_dropdown( array(
-						'wrapper_class' => 'ea-col-6',
-						'label'         => __( 'Category', 'wp-ever-accounting' ),
-						'name'          => 'category_id',
-						'value'         => $invoice->get_category_id(),
-						'required'      => true,
-						'type'          => 'expense',
-						'creatable'     => true,
+					'wrapper_class' => 'ea-col-6',
+					'label'         => __( 'Category', 'wp-ever-accounting' ),
+					'name'          => 'category_id',
+					'value'         => $invoice->get_category_id(),
+					'required'      => true,
+					'type'          => 'expense',
+					'creatable'     => true,
 				) );
 				eaccounting_file_input( array(
-						'label'         => __( 'Attachments', 'wp-ever-accounting' ),
-						'name'          => 'attachment',
-						'value'         => $invoice->get_attachment(),
-						'required'      => false,
-						'wrapper_class' => 'ea-col-6',
-						'placeholder'   => __( 'Upload File', 'wp-ever-accounting' ),
+					'label'         => __( 'Attachments', 'wp-ever-accounting' ),
+					'name'          => 'attachment',
+					'value'         => $invoice->get_attachment(),
+					'required'      => false,
+					'wrapper_class' => 'ea-col-6',
+					'placeholder'   => __( 'Upload File', 'wp-ever-accounting' ),
+				) );
+				eaccounting_hidden_input( array(
+					'name'  => 'id',
+					'value' => $invoice->get_id(),
+				));
+
+				eaccounting_hidden_input( array(
+					'name'  => 'action',
+					'value' => 'eaccounting_edit_invoice',
 				) );
 				?>
 			</div>
