accb7a172375548b376a66e44bdb3d8c472b64f6
Sultan Nasir Uddin <manikdrmc@gmail.com>
2021-01-12 10:57:26 +0600

    Allow decimal numbers

--- includes/admin/views/bills/bill-item.php
+++ includes/admin/views/bills/bill-item.php
@@ -57,7 +57,7 @@ defined( 'ABSPATH' ) || exit;
 				<abbr title="<?php echo esc_html( eaccounting_price( $item->get_tax(), $bill->get_currency_code() ) ); ?>"><?php echo esc_html( number_format_i18n( $item->get_tax_rate(), 2 ) ); ?><small>%</small></abbr>
 			</div>
 			<div class="edit" style="display: none;">
-				<input type="number" step="0.05" min="0" max="1000" class="line_item_tax" name="items[<?php echo esc_attr( $item_id ); ?>][tax_rate]" value="<?php echo esc_attr( $item->get_tax_rate() ); ?>">
+				<input type="number" step="0.01" min="0" max="1000" class="line_item_tax" name="items[<?php echo esc_attr( $item_id ); ?>][tax_rate]" value="<?php echo esc_attr( $item->get_tax_rate() ); ?>">
 			</div>
 		</td>
 	<?php endif; ?>
--- includes/admin/views/bills/edit-bill.php
+++ includes/admin/views/bills/edit-bill.php
@@ -195,7 +195,7 @@ $due_date = date_i18n( 'Y-m-d', strtotime( "+ $due days", current_time( 'timesta
 				'value'    => 0.0000,
 				'required' => true,
 				'attr'     => array(
-					'step' => 0.1,
+					'step' => 0.01,
 					'min'  => 0,
 				),
 			)
