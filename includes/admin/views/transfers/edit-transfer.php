dff034f8b77685413a67e867c4b0bdbd9c12e681
sudipto-me <sudipto004.ruet@gmail.com>
2021-01-14 11:22:36 +0600

    Fixed #267

--- includes/admin/views/transfers/edit-transfer.php
+++ includes/admin/views/transfers/edit-transfer.php
@@ -40,95 +40,95 @@ try {
 			<div class="ea-row">
 				<?php
 				eaccounting_account_dropdown(
-						array(
-								'wrapper_class' => 'ea-col-6',
-								'label'         => __( 'From Account', 'wp-ever-accounting' ),
-								'name'          => 'from_account_id',
-								'value'         => $transfer->get_from_account_id(),
-								'required'      => true,
-								'placeholder'   => __( 'Select Account', 'wp-ever-accounting' ),
-								'creatable'     => true,
-						)
+					array(
+						'wrapper_class' => 'ea-col-6',
+						'label'         => __( 'From Account', 'wp-ever-accounting' ),
+						'name'          => 'from_account_id',
+						'value'         => $transfer->get_from_account_id(),
+						'required'      => true,
+						'placeholder'   => __( 'Select Account', 'wp-ever-accounting' ),
+						'creatable'     => true,
+					)
 				);
 
 				eaccounting_account_dropdown(
-						array(
-								'wrapper_class' => 'ea-col-6',
-								'label'         => __( 'To Account', 'wp-ever-accounting' ),
-								'name'          => 'to_account_id',
-								'value'         => $transfer->get_to_account_id(),
-								'default'       => '',
-								'required'      => true,
-								'placeholder'   => __( 'Select Account', 'wp-ever-accounting' ),
-								'creatable'     => true,
-						)
+					array(
+						'wrapper_class' => 'ea-col-6',
+						'label'         => __( 'To Account', 'wp-ever-accounting' ),
+						'name'          => 'to_account_id',
+						'value'         => $transfer->get_to_account_id(),
+						'default'       => '',
+						'required'      => true,
+						'placeholder'   => __( 'Select Account', 'wp-ever-accounting' ),
+						'creatable'     => true,
+					)
 				);
 
 				eaccounting_text_input(
-						array(
-								'label'         => __( 'Amount', 'wp-ever-accounting' ),
-								'name'          => 'amount',
-								'value'         => $transfer->get_amount(),
-								'data_type'     => 'price',
-								'required'      => true,
-								'wrapper_class' => 'ea-col-6',
-								'placeholder'   => __( 'Enter amount', 'wp-ever-accounting' ),
-						)
+					array(
+						'label'         => __( 'Amount', 'wp-ever-accounting' ),
+						'name'          => 'amount',
+						'value'         => $transfer->get_amount(),
+						'data_type'     => 'price',
+						'required'      => true,
+						'wrapper_class' => 'ea-col-6',
+						'placeholder'   => __( 'Enter amount', 'wp-ever-accounting' ),
+					)
 				);
 
 				eaccounting_text_input(
-						array(
-								'wrapper_class' => 'ea-col-6',
-								'label'         => __( 'Date', 'wp-ever-accounting' ),
-								'name'          => 'date',
-								'placeholder'   => __( 'Enter date', 'wp-ever-accounting' ),
-								'data_type'     => 'date',
-								'value'         => $transfer->get_date() ? $transfer->get_date() : null,
-								'required'      => true,
-						)
+					array(
+						'wrapper_class' => 'ea-col-6',
+						'label'         => __( 'Date', 'wp-ever-accounting' ),
+						'name'          => 'date',
+						'placeholder'   => __( 'Enter date', 'wp-ever-accounting' ),
+						'data_type'     => 'date',
+						'value'         => $transfer->get_date() ? eaccounting_date( $transfer->get_date(), 'Y-m-d' ) : null,
+						'required'      => true,
+					)
 				);
 				eaccounting_payment_method_dropdown(
-						array(
-								'label'         => __( 'Payment Method', 'wp-ever-accounting' ),
-								'name'          => 'payment_method',
-								'placeholder'   => __( 'Enter payment method', 'wp-ever-accounting' ),
-								'wrapper_class' => 'ea-col-6',
-								'required'      => true,
-								'value'         => $transfer->get_payment_method(),
-						)
+					array(
+						'label'         => __( 'Payment Method', 'wp-ever-accounting' ),
+						'name'          => 'payment_method',
+						'placeholder'   => __( 'Enter payment method', 'wp-ever-accounting' ),
+						'wrapper_class' => 'ea-col-6',
+						'required'      => true,
+						'value'         => $transfer->get_payment_method(),
+					)
 				);
 				eaccounting_text_input(
-						array(
-								'label'         => __( 'Reference', 'wp-ever-accounting' ),
-								'name'          => 'reference',
-								'value'         => $transfer->get_reference(),
-								'required'      => false,
-								'wrapper_class' => 'ea-col-6',
-								'placeholder'   => __( 'Enter reference', 'wp-ever-accounting' ),
-						)
+					array(
+						'label'         => __( 'Reference', 'wp-ever-accounting' ),
+						'name'          => 'reference',
+						'value'         => $transfer->get_reference(),
+						'required'      => false,
+						'wrapper_class' => 'ea-col-6',
+						'placeholder'   => __( 'Enter reference', 'wp-ever-accounting' ),
+					)
 				);
 				eaccounting_textarea(
-						array(
-								'label'         => __( 'Description', 'wp-ever-accounting' ),
-								'name'          => 'description',
-								'value'         => $transfer->get_description(),
-								'required'      => false,
-								'wrapper_class' => 'ea-col-12',
-								'placeholder'   => __( 'Enter description', 'wp-ever-accounting' ),
-						)
+					array(
+						'label'         => __( 'Description', 'wp-ever-accounting' ),
+						'name'          => 'description',
+						'value'         => $transfer->get_description(),
+						'required'      => false,
+						'wrapper_class' => 'ea-col-12',
+						'placeholder'   => __( 'Enter description', 'wp-ever-accounting' ),
+					)
 				);
 
 				eaccounting_hidden_input(
-						array(
-								'name'  => 'id',
-								'value' => $transfer->get_id(),
-						)
+					array(
+						'name'  => 'id',
+						'value' => $transfer->get_id(),
+					)
 				);
 				eaccounting_hidden_input(
-						array(
-								'name'  => 'action',
-								'value' => 'eaccounting_edit_transfer',
-						)
+					array(
+						'name'  => 'action',
+						'value' => 'eaccounting_edit_transfer',
+					)
 				);
 				?>
 			</div>
