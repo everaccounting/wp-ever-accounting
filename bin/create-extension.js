f315b25c57bfe39e073ab0bf2e3e9dac167ff6df
sultann <manikdrmc@gmail.com>
2021-03-31 01:30:02 +0600

    Fix extension create

--- bin/create-extension.js
+++ bin/create-extension.js
@@ -5,6 +5,7 @@ const path = require( 'path' );
 const promptly = require( 'promptly' );
 const chalk = require( 'chalk' );
 const clone = require( 'git-clone' );
+const replace = require( 'replace-in-file' );
 const repo = 'git@github.com:everaccounting/eaccounting-addon.git';
 
 ( async () => {
@@ -12,19 +13,87 @@ const repo = 'git@github.com:everaccounting/eaccounting-addon.git';
 	console.log(
 		chalk.green( '🎉 Welcome to EverAccounting Extension Generator 🎉' )
 	);
-	const extensionName = await promptly.prompt(
+	// Addon
+	// addon
+	// ADDON
+	const inputName = await promptly.prompt(
 		chalk.green(
 			'What is the name of your extension? eg. for Ever Accounting - Slack, type only slack'
 		)
 	);
 
-	const directoryDashed = 'eaccounting-' + extensionName;
+	const nameCapitalized = inputName.replace(
+		/\b\w+/g,
+		( l ) => l.charAt( 0 ).toUpperCase() + l.slice( 1 )
+	);
+	const nameCamelCase = nameCapitalized.replace( / /g, '' );
+	const nameUnderscored = nameCapitalized
+		.replace( /[^a-zA-Z]/g, '_' )
+		.toLowerCase();
+	const nameDashed = nameUnderscored.replace( /_/g, '-' );
+	const nameSpaces = nameCapitalized.replace( / /g, '_' );
+	const nameCaps = nameCamelCase.toUpperCase();
+	const directoryName = 'eaccounting-' + nameDashed;
+	const directoryPath = '../' + directoryName;
+	const textToReplace = [
+		{
+			from: /Ever Accounting - Addon/g,
+			to: `Ever Accounting - ${ nameCapitalized }`,
+		},
+		{
+			from: /EverAccounting_Addon/g,
+			to: `EverAccounting_${ nameCamelCase }`,
+		},
+		{
+			from: /EACCOUNTING_ADDON/g,
+			to: `EACCOUNTING_${ nameCaps }`,
+		},
+		{
+			from: /eaccounting-addon/g,
+			to: `eaccounting-${ nameDashed }`,
+		},
+		{
+			from: /eaccounting_addon/g,
+			to: `eaccounting_${ nameUnderscored }`,
+		},
+		{
+			from: /addon/g,
+			to: nameDashed,
+		},
+		{
+			from: /Addon/g,
+			to: nameSpaces,
+		},
+	];
 
-	if ( fs.existsSync( '../' + directoryDashed ) ) {
+	// An array of directories to remove
+	const directoriesToRemove = [ '.git' ];
+
+	// Objects of directories that need to be renamed
+	const directoriesToRename = [
+		{
+			from: 'eaccounting-addon.php',
+			to: directoryName + '.php',
+		},
+		{
+			from: 'eaccounting-addon',
+			to: directoryName,
+		},
+		{
+			from: 'languages/eaccounting-addon.pot',
+			to: 'languages/' + directoryName + '.pot',
+		},
+		{
+			from: 'languages/eaccounting-addon.po',
+			to: 'languages/' + directoryName + '.po',
+		},
+	];
+
+	if ( fs.existsSync( directoryPath ) ) {
 		console.log(
 			chalk.yellow.bold( '✘ Warning: ' ) +
 				'"' +
-				directoryDashed +
+				directoryName +
 				'" directory already exists, please remove it or change the path'
 		);
 
@@ -37,11 +106,59 @@ const repo = 'git@github.com:everaccounting/eaccounting-addon.git';
 	}
 
 	// Clone the repo and get to work
-	clone( repo, '../' + directoryDashed, function ( err ) {
+	clone( repo, '../' + directoryName, function ( err ) {
 		if ( err ) {
 			console.log( err );
 		} else {
 			console.log( chalk.green( '✔ Clone Successful' ) );
+
+			// Delete unnecessary directories
+			if ( directoriesToRemove.length ) {
+				directoriesToRemove.forEach( function ( dir ) {
+					if ( fs.existsSync( directoryPath + '/' + dir ) ) {
+						deleteDirectory(
+							directoryPath + '/' + dir,
+							function () {
+								console.log(
+									chalk.green( `✔ ${ dir } deleted` )
+								);
+							}
+						);
+					}
+				} );
+			}
+
+			// Synchronously find and replace text within files
+			textToReplace.forEach( function ( text ) {
+				try {
+					const changes = replace.sync( {
+						files: directoryPath + '/**/*.*',
+						from: text.from,
+						to: text.to,
+						encoding: 'utf8',
+					} );
+
+					console.log( chalk.green.bold( `✔ Modified files` ) );
+				} catch ( error ) {
+					console.error( 'Error occurred:', error );
+				}
+			} );
+
+			// Rename directories
+			directoriesToRename.forEach( function ( dir ) {
+				if ( fs.existsSync( directoryPath + '/' + dir.from ) ) {
+					fs.rename(
+						directoryPath + '/' + dir.from,
+						directoryPath + '/' + dir.to,
+						function ( err ) {
+							if ( err ) throw err;
+							console.log(
+								chalk.green( '✔ Renamed ' + dir.from )
+							);
+						}
+					);
+				}
+			} );
 		}
 	} ); // clone()
 } )();
