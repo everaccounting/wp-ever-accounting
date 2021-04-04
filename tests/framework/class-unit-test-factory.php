5137a5a4102e5bfd5a80f72f7ed5514df7aa8ed4
sudipto-me <sudipto004.ruet@gmail.com>
2021-04-04 16:17:58 +0600

    category factory added

--- tests/framework/class-unit-test-factory.php
+++ tests/framework/class-unit-test-factory.php
@@ -8,6 +8,8 @@
 
 namespace EverAccounting\Tests\Framework;
 
+use EverAccounting\Tests\Framework\Factories\Account_Factory;
+use EverAccounting\Tests\Framework\Factories\Category_Factory;
 use EverAccounting\Tests\Framework\Factories\Customer_Factory;
 use EverAccounting\Tests\Framework\Factories\Vendor_Factory;
 
@@ -19,6 +21,8 @@ class Factory extends \WP_UnitTest_Factory {
 	 */
 	public $customer;
 	public $vendor;
+	public $account;
+	public $category;
 
 	/**
 	 * Setup factories.
@@ -28,5 +32,7 @@ class Factory extends \WP_UnitTest_Factory {
 
 		$this->customer = new Customer_Factory( $this );
 		$this->vendor = new Vendor_Factory( $this );
+		$this->account = new Account_Factory( $this );
+		$this->category = new Category_Factory( $this );
 	}
 }
