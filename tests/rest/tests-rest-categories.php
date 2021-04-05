7f653bfa476ed62d7fb9c5e8de664accfe500967
sudipto <sudipto004.ruet@gmail.com>
2021-04-05 18:50:47 +0600

    category rest controller updated

--- tests/rest/tests-rest-categories.php
+++ tests/rest/tests-rest-categories.php
@@ -56,11 +56,83 @@ class Tests_REST_Categories extends REST_UnitTestCase {
 	 *
 	 * @since 1.1.4
 	 */
+	public function test_get_categories_without_permission() {
+		wp_set_current_user( 0 );
+		$category = Category_Helper::create_category();
+		$response = $this->do_rest_get_request( '/ea/v1/categories/' . $category->get_id() );
+		$this->assertEquals( 401, $response->get_status() );
+	}
+
+	/**
+	 * Test getting a single category.
+	 *
+	 * @since 1.1.4
+	 */
+	public function test_get_category() {
+		wp_set_current_user( $this->user->ID );
+		$expected_response_fields = array_keys( $this->endpoint->get_public_item_schema()['properties'] );
+		$category                 = Category_Helper::create_category();
+		$response                 = $this->do_rest_get_request( '/ea/v1/categories/' . $category->get_id() );
+		$this->assertEquals( 200, $response->get_status(), var_export( $response, true ) );
+		$response_fields = array_keys( $response->get_data() );
+
+		$this->assertEmpty( array_diff( $expected_response_fields, $response_fields ), 'These fields were expected but not present in API response: ' . print_r( array_diff( $expected_response_fields, $response_fields ), true ) );
+
+		$this->assertEmpty( array_diff( $response_fields, $expected_response_fields ), 'These fields were not expected in the API response: ' . print_r( array_diff( $response_fields, $expected_response_fields ), true ) );
+
+		// Test that all fields are returned when requested one by one.
+		foreach ( $expected_response_fields as $field ) {
+			$request = new WP_REST_Request( 'GET', '/ea/v1/categories/' . $category->get_id() );
+			$request->set_param( '_fields', $field );
+			$response = $this->server->dispatch( $request );
+			$this->assertEquals( 200, $response->get_status() );
+			$response_fields = array_keys( $response->get_data() );
+			$this->assertContains( $field, $response_fields, "Field $field was expected but not present in order API response." );
+		}
+	}
+
+	/**
+	 * Test getting single category without permission.
+	 *
+	 * @since 1.1.4
+	 */
 	public function test_get_category_without_permission() {
 		wp_set_current_user( 0 );
 		$category = Category_Helper::create_category();
 		$response = $this->do_rest_get_request( '/ea/v1/categories/' . $category->get_id() );
 		$this->assertEquals( 401, $response->get_status() );
+	}
+
+	/**
+	 * Test creating a single category.
+	 *
+	 * @since 1.1.4
+	 */
+	public function test_insert_category() {
+		wp_set_current_user( $this->user->ID );
+		$data     = Category_Helper::create_category( false );
+		$response = $this->do_rest_post_request( '/ea/v1/categories', $data );
+		$category = $response->get_data();
+		$this->assertEquals( 201, $response->get_status() );
+		unset( $category['id'] );
+		foreach ( $category as $key => $value ) {
+			if ( ! is_scalar( $value ) ) {
+				continue;
+			}
+			$this->assertEquals( $value, $data[ $key ] );
+		}
+	}
+
+	/**
+	 * Test getting single category without permission.
+	 *
+	 * @since 1.1.4
+	 */
+	public function test_insert_category_without_permission() {
+		wp_set_current_user( 0 );
+		$data = Category_Helper::create_category( false );
+		$response = $this->do_rest_post_request( '/ea/v1/categories/' , $data );
+		$this->assertEquals( 404, $response->get_status() );
 	}
 
 	/**
