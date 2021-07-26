426445f00a300391b1bc2509cc5c973ea6ec88f8
sudipto <sudipto004.ruet@gmail.com>
2021-07-26 17:58:13 +0600

    currency class added

--- includes/class-currency.php
+++ includes/class-currency.php
@@ -3,7 +3,7 @@
  * Handle the Currency object.
  *
  * @package     EverAccounting
- * @class       Contact
+ * @class       Currency
  * @version     1.2.1
  */
 
@@ -13,11 +13,22 @@ defined( 'ABSPATH' ) || exit;
 
 /**
  * Core class used to implement the Currency object.
+ *
  * @package EverAccounting
  *
  * @since 1.2.1
  *
  * @property string $name
+ * @property string $code
+ * @property float $rate
+ * @property int $precision
+ * @property string $symbol
+ * @property int $subunit
+ * @property string $position
+ * @property string $decimal_separator
+ * @property string $thousand_separator
+ * @property boolean $enabled
+ * @property string $date_created
  */
 class Currency {
 	/**
@@ -36,9 +47,12 @@ class Currency {
 	 */
 	public $id = null;
 
-
 	/**
 	 * Currency constructor.
+	 *
+	 * @param object $currency Currency object
+	 *
+	 * @since 1.2.1
 	 */
 	public function __construct( $currency ) {
 		if ( $currency instanceof self ) {
@@ -70,7 +84,6 @@ class Currency {
 	 *
 	 * @return object|false Raw currency object
 	 * @global \wpdb $wpdb WordPress database abstraction object.
-	 *
 	 */
 	public static function load( $id ) {
 		global $wpdb;
@@ -79,14 +92,14 @@ class Currency {
 			return false;
 		}
 
-		$data = wp_cache_get( $id, 'ea_currencys' );
+		$data = wp_cache_get( $id, 'ea_currencies' );
 		if ( $data ) {
 			return $data;
 		}
 
 		$data = $wpdb->get_row(
 			$wpdb->prepare(
-				"SELECT * FROM {$wpdb->prefix}ea_currencys WHERE id = %d LIMIT 1",
+				"SELECT * FROM {$wpdb->prefix}ea_currencies WHERE id = %d LIMIT 1",
 				$id
 			)
 		);
@@ -95,7 +108,7 @@ class Currency {
 			return false;
 		}
 
-		eaccounting_set_cache( 'currency', $data );
+		eaccounting_set_cache( 'ea_currencies', $data );
 
 		return $data;
 	}
@@ -105,57 +118,49 @@ class Currency {
 	 *
 	 * @param string $key Contact field to check if set.
 	 *
-	 * @return bool Whether the given Contact field is set.
+	 * @return bool Whether the given Currency field is set.
 	 * @since 1.2.1
-	 *
 	 */
 	public function __isset( $key ) {
 		if ( isset( $this->data->$key ) ) {
 			return true;
 		}
 
-		return metadata_exists( 'currency', $this->id, $key );
+		return false;
 	}
 
 	/**
 	 * Magic method for setting currency fields.
 	 *
-	 * This method does not update custom fields in the database. It only stores
-	 * the value on the WP_User instance.
+	 * This method does not update custom fields in the database.
 	 *
-	 * @param string $key Contact key.
-	 * @param mixed $value Contact value.
+	 * @param string $key Currency key.
+	 * @param mixed  $value Currency value.
 	 *
 	 * @since 1.2.1
-	 *
 	 */
 	public function __set( $key, $value ) {
 		if ( is_callable( array( $this, 'set_' . $key ) ) ) {
 			$this->$key( $value );
-		} else if ( isset( $this->data->$key ) ) {
-			$this->data->$key = $value;
 		} else {
-			$this->update_meta( $key, $value );
+			$this->data->$key = $value;
 		}
 	}
 
 	/**
 	 * Magic method for accessing custom fields.
 	 *
-	 * @param string $key User field to retrieve.
+	 * @param string $key Currency field to retrieve.
 	 *
-	 * @return mixed Value of the given Contact field (if set).
+	 * @return mixed Value of the given Currency field (if set).
 	 * @since 1.2.1
-	 *
 	 */
 	public function __get( $key ) {
 
 		if ( is_callable( array( $this, 'get_' . $key ) ) ) {
 			$value = $this->$key();
-		} else if ( isset( $this->data->$key ) ) {
-			$value = $this->data->$key;
 		} else {
-			$value = $this->get_meta( $key, true );
+			$value = $this->data->$key;
 		}
 
 		return $value;
@@ -164,10 +169,9 @@ class Currency {
 	/**
 	 * Magic method for unsetting a certain field.
 	 *
-	 * @param string $key Contact key to unset.
+	 * @param string $key Currency key to unset.
 	 *
 	 * @since 1.2.1
-	 *
 	 */
 	public function __unset( $key ) {
 		if ( isset( $this->data->$key ) ) {
@@ -176,42 +180,14 @@ class Currency {
 	}
 
 	/**
-	 * Get meta data.
-	 *
-	 * @param string $meta_key
-	 * @param false $single
-	 *
-	 * @return array|false|mixed
-	 * @since 1.2.1
-	 */
-	protected function get_meta( $meta_key = '', $single = true ) {
-		return get_metadata( 'currency', $this->id, $meta_key, $single );
-	}
-
-	/**
-	 * Update meta value.
-	 *
-	 * @param $meta_key
-	 * @param $meta_value
-	 * @param string $prev_value
-	 *
-	 * @return bool|int
-	 * @since 1.2.1
-	 */
-	protected function update_meta( $meta_key, $meta_value, $prev_value = '' ) {
-		return update_metadata( 'currency', $this->id, $meta_key, $meta_value, $prev_value );
-	}
-
-	/**
 	 * Determine whether a property or meta key is set
 	 *
-	 * Consults the users and currency_meta tables.
+	 * Consults the currencies tables
 	 *
 	 * @param string $key Property
 	 *
 	 * @return bool
 	 * @since 1.2.1
-	 *
 	 */
 	public function has_prop( $key ) {
 		return $this->__isset( $key );
@@ -222,7 +198,6 @@ class Currency {
 	 *
 	 * @return bool True if currency exists in the database, false if not.
 	 * @since 1.2.1
-	 *
 	 */
 	public function exists() {
 		return ! empty( $this->id );
@@ -233,9 +208,59 @@ class Currency {
 	 *
 	 * @return array Array representation.
 	 * @since 1.2.1
-	 *
 	 */
 	public function to_array() {
 		return get_object_vars( $this->data );
+	}
+
+	/**
+	 * getPrefix.
+	 *
+	 * @return string
+	 * @since 1.0.2
+	 */
+	public function get_calculated_prefix() {
+		if ( ! $this->is_symbol_first() ) {
+			return '';
+		}
+
+		return $this->symbol;
+	}
+
+	/**
+	 * getSuffix.
+	 *
+	 * @return string
+	 * @since 1.0.2
+	 */
+	public function get_calculated_suffix() {
+		if ( $this->is_symbol_first() ) {
+			return '';
+		}
+
+		return ' ' . $this->symbol;
+	}
+
+	/**
+	 * equals.
+	 *
+	 * @param object $currency Currency object
+	 *
+	 * @return bool
+	 * @since 1.0.2
+	 */
+	public function currency_equals( $currency ) {
+		return $this->get_code === $currency->code;
+	}
+
+	/**
+	 * is_symbol_first.
+	 *
+	 * @since 1.0.2
+	 *
+	 * @return bool
+	 */
+	public function is_symbol_first() {
+		return 'before' === $this->position;
 	}
 }
