7e6498a4519467f1cc70f473306afb42e1054c3d
sudipto <sudipto004.ruet@gmail.com>
2021-07-26 16:30:34 +0600

    category added

---
+++ includes/class-category.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Handle the Category object.
+ *
+ * @package     EverAccounting
+ * @class       Category
+ * @version     1.2.1
+ */
+
+namespace EverAccounting;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Core class used to implement the Contact object.
+ *
+ * @package EverAccounting
+ *
+ * @since 1.2.1
+ *
+ * @property int $user_id
+ * @property string $name
+ * @property string $company
+ * @property string $email
+ * @property string $phone
+ * @property string $website
+ * @property string $vat_number
+ * @property string $birth_date
+ * @property string $street
+ * @property string $city
+ * @property string $state
+ * @property string $postcode
+ * @property string $country
+ * @property string $type
+ * @property string $currency_code
+ * @property string $thumbnail_id
+ * @property string $enabled
+ * @property int $creator_id
+ * @property string $date_created
+ */
