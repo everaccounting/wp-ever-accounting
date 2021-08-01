62979424bf3ec808a37a20a1f70e8f9542d8700a
sudipto <sudipto004.ruet@gmail.com>
2021-08-01 16:47:28 +0600

    contact functions updated

--- includes/ea-contact-functions.php
+++ includes/ea-contact-functions.php
@@ -32,8 +32,8 @@ function eaccounting_get_contact_types() {
  * Retrieves contact data given a contact id or contact object.
  *
  * @param int|object|Contact $contact contact to retrieve
- * @param string $output The required return type. One of OBJECT, ARRAY_A, or ARRAY_N.Default OBJECT.
- * @param string $filter Type of filter to apply. Accepts 'raw', 'edit', 'db', or 'display'. Default 'raw'.
+ * @param string             $output The required return type. One of OBJECT, ARRAY_A, or ARRAY_N.Default OBJECT.
+ * @param string             $filter Type of filter to apply. Accepts 'raw', 'edit', 'db', or 'display'. Default 'raw'.
  *
  * @return Contact|array|null
  * @since 1.1.0
@@ -69,95 +69,91 @@ function eaccounting_get_contact( $contact, $output = OBJECT, $filter = 'raw' ) 
 }
 
 /**
- *  Create new contact programmatically.
+ *  Insert or update a contact.
  *
- * @param array|object|Contact $contact_data An array, object, or Contact object of data arguments.
+ * @param array|object|Contact $contact_arr An array, object, or contact object of data arguments.
  *
- * @return Contact|WP_Error The Contact object or WP_Error otherwise.
+ * @return Contact|WP_Error The contact object or WP_Error otherwise.
  * @global wpdb $wpdb WordPress database abstraction object.
  * @since 1.1.0
  */
-function eaccounting_insert_contact( $contact_data ) {
+function eaccounting_insert_contact( $contact_arr ) {
 	global $wpdb;
 	$user_id = get_current_user_id();
-	if ( $contact_data instanceof Contact ) {
-		$contact_data = $contact_data->to_array();
-	} elseif ( $contact_data instanceof stdClass ) {
-		$contact_data = get_object_vars( $contact_data );
+	if ( $contact_arr instanceof Contact ) {
+		$contact_arr = $contact_arr->to_array();
+	} elseif ( $contact_arr instanceof stdClass ) {
+		$contact_arr = get_object_vars( $contact_arr );
 	}
 
 	$defaults = array(
-		'name'           => '',
-		'sku'            => '',
-		'thumbnail_id'   => '',
-		'description'    => '',
-		'sale_price'     => 0.0000,
-		'purchase_price' => 0.0000,
-		'quantity'       => 1,
-		'category_id'    => '',
-		'sales_tax'      => '',
-		'purchase_tax'   => '',
-		'enabled'        => true,
-		'creator_id'     => $user_id,
-		'date_created'   => '',
+		'user_id'       => null,
+		'name'          => '',
+		'company'       => '',
+		'email'         => '',
+		'phone'         => '',
+		'birth_date'    => '',
+		'street'        => '',
+		'city'          => '',
+		'state'         => '',
+		'postcode'      => '',
+		'country'       => '',
+		'website'       => '',
+		'vat_number'    => '',
+		'currency_code' => '',
+		'type'          => '',
+		'thumbnail_id'  => null,
+		'enabled'       => true,
+		'creator_id'    => $user_id,
+		'date_created'  => null,
 	);
 
 	// Are we updating or creating?
-	$id      = null;
-	$update  = false;
-	$changes = $contact_data;
-	if ( ! empty( $contact_data['id'] ) ) {
-		$update = true;
-		$id     = absint( $contact_data['id'] );
-		$before = eaccounting_get_contact( $id );
+	$id          = null;
+	$update      = false;
+	$data_before = array();
+	if ( ! empty( $item_data['id'] ) ) {
+		$update      = true;
+		$id          = absint( $item_data['id'] );
+		$data_before = eaccounting_get_contact( $id, ARRAY_A );
 
-		if ( is_null( $before ) ) {
-			return new WP_Error( 'invalid_contact_id', __( 'Invalid item id to update.' ) );
+		if ( is_null( $data_before ) ) {
+			return new WP_Error( 'invalid_contact_id', __( 'Invalid contact id to update.', 'wp-ever-accounting' ) );
 		}
-		// Store changes value.
-		$changes = array_diff_assoc( $contact_data, $before->to_array() );
 
 		// Merge old and new fields with new fields overwriting old ones.
-		$contact_data = array_merge( $before->to_array(), $contact_data );
+		$contact_arr = array_merge( $data_before, $contact_arr );
+		$data_before = $data_before->to_array();
 	}
 
-	$data_arr = wp_parse_args( $contact_data, $defaults );
-	$data_arr = eaccounting_sanitize_contact( $data_arr, 'db' );
+	$item_data = wp_parse_args( $contact_arr, $defaults );
+	$data_arr  = eaccounting_sanitize_contact( $contact_arr, 'db' );
 
+	// Check required
 	if ( empty( $data_arr['name'] ) ) {
 		return new WP_Error( 'invalid_contact_name', esc_html__( 'Contact name is required', 'wp-ever-accounting' ) );
+	}
+
+	if ( empty( $data_arr['currency_code'] ) ) {
+		return new WP_Error( 'invalid_currency_code', esc_html__( 'Contact currency code is required', 'wp-ever-accounting' ) );
 	}
 
 	if ( empty( $data_arr['date_created'] ) || '0000-00-00 00:00:00' === $data_arr['date_created'] ) {
 		$data_arr['date_created'] = current_time( 'mysql' );
 	}
 
-	// Compute fields.
-	$name           = $data_arr['name'];
-	$sku            = $data_arr['sku'];
-	$description    = $data_arr['description'];
-	$sale_price     = $data_arr['sale_price'];
-	$purchase_price = $data_arr['purchase_price'];
-	$quantity       = $data_arr['quantity'];
-	$category_id    = $data_arr['category_id'];
-	$sales_tax      = $data_arr['sales_tax'];
-	$purchase_tax   = $data_arr['purchase_tax'];
-	$thumbnail_id   = (int) $data_arr['thumbnail_id'];
-	$enabled        = (int) $data_arr['enabled'];
-	$creator_id     = (int) $data_arr['creator_id'];
-	$date_created   = $data_arr['date_created'];
-	$data           = compact( 'name', 'sku', 'description', 'sale_price', 'purchase_price', 'quantity', 'category_id', 'sales_tax', 'purchase_tax', 'thumbnail_id', 'enabled', 'creator_id', 'date_created' );
+	$fields = array_keys( $defaults );
+	$data   = wp_array_slice_assoc( $data_arr, $fields );
 
 	/**
 	 * Filters contact data before it is inserted into the database.
 	 *
-	 * @param array $data Contact data to be inserted.
-	 * @param array $data_arr Sanitized account data.
-	 * @param array $contact_data Contact data as originally passed to the function.
+	 * @param array $data Data to be inserted.
+	 * @param array $data_arr Sanitized data.
 	 *
 	 * @since 1.2.1
 	 */
-	$data = apply_filters( 'eaccounting_insert_contact_data', $data, $data_arr, $contact_data );
+	$data = apply_filters( 'eaccounting_insert_contact', $data, $data_arr );
 
 	$data  = wp_unslash( $data );
 	$where = array( 'id' => $id );
@@ -171,12 +167,13 @@ function eaccounting_insert_contact( $contact_data ) {
 		 * @param array $data Contact data to be inserted.
 		 * @param array $changes Contact data to be updated.
 		 * @param array $data_arr Sanitized contact data.
+		 * @param array $data_before Contact previous data.
 		 *
 		 * @since 1.2.1
 		 */
-		do_action( 'eaccounting_pre_update_contact', $id, $data, $changes, $data_arr );
-		if ( false === $wpdb->update( $wpdb->prefix . 'ea_contacts', $data, $where ) ) {
-			new WP_Error( 'db_update_error', __( 'Could not update contact in the database.' ), $wpdb->last_error );
+		do_action( 'eaccounting_pre_update_contact', $id, $data, $data_arr, $data_before );
+		if ( false === $wpdb->update( $wpdb->prefix . 'ea_contacts', $data, $where, $data_before ) ) {
+			new WP_Error( 'db_update_error', __( 'Could not update contact in the database.', 'wp-ever-accounting' ), $wpdb->last_error );
 		}
 
 		/**
@@ -185,11 +182,12 @@ function eaccounting_insert_contact( $contact_data ) {
 		 * @param int $id Contact id.
 		 * @param array $data Contact data to be inserted.
 		 * @param array $changes Contact data to be updated.
-		 * @param array $data_arr Sanitized contact data.
+		 * @param array $data_arr Sanitized Contact data.
+		 * @param array $data_before Contact previous data.
 		 *
 		 * @since 1.2.1
 		 */
-		do_action( 'eaccounting_update_contact', $id, $data, $changes, $data_arr );
+		do_action( 'eaccounting_update_contact', $id, $data, $data_arr, $data_before );
 	} else {
 
 		/**
@@ -197,14 +195,14 @@ function eaccounting_insert_contact( $contact_data ) {
 		 *
 		 * @param array $data Contact data to be inserted.
 		 * @param string $data_arr Sanitized contact data.
-		 * @param array $contact_data Contact data as originally passed to the function.
+		 * @param array $item_data Contact data as originally passed to the function.
 		 *
 		 * @since 1.2.1
 		 */
-		do_action( 'eaccounting_pre_insert_contact', $data, $data_arr, $contact_data );
+		do_action( 'eaccounting_pre_insert_contact', $data, $data_arr, $item_data );
 
 		if ( false === $wpdb->insert( $wpdb->prefix . 'ea_contacts', $data ) ) {
-			new WP_Error( 'db_insert_error', __( 'Could not insert contact into the database.' ), $wpdb->last_error );
+			new WP_Error( 'db_insert_error', __( 'Could not insert contact into the database.', 'wp-ever-accounting' ), $wpdb->last_error );
 		}
 
 		$id = (int) $wpdb->insert_id;
@@ -215,11 +213,11 @@ function eaccounting_insert_contact( $contact_data ) {
 		 * @param int $id Contact id.
 		 * @param array $data Contact has been inserted.
 		 * @param array $data_arr Sanitized contact data.
-		 * @param array $contact_data Contact data as originally passed to the function.
+		 * @param array $item_data Contact data as originally passed to the function.
 		 *
 		 * @since 1.2.1
 		 */
-		do_action( 'eaccounting_insert_contact', $id, $data, $data_arr, $contact_data );
+		do_action( 'eaccounting_insert_contact', $id, $data, $data_arr, $item_data );
 	}
 
 	// Clear cache.
@@ -230,7 +228,7 @@ function eaccounting_insert_contact( $contact_data ) {
 	$contact = eaccounting_get_contact( $id );
 
 	/**
-	 * Fires once an contact has been saved.
+	 * Fires once a contact has been saved.
 	 *
 	 * @param int $id Contact id.
 	 * @param Contact $contact Contact object.
@@ -238,15 +236,15 @@ function eaccounting_insert_contact( $contact_data ) {
 	 *
 	 * @since 1.2.1
 	 */
-	do_action( 'eaccounting_saved_contact', $id, $contact, $update );
+	do_action( 'eaccounting_saved_contact', $id, $contact, $update, $data_arr, $data_before );
 
 	return $contact;
 }
 
 /**
- * Delete an contact.
+ * Delete a contact.
  *
- * @param int $contact_id Contact ID
+ * @param int $contact_id Contact id.
  *
  * @return Contact |false|null Contact data on success, false or null on failure.
  * @since 1.1.0
@@ -273,10 +271,10 @@ function eaccounting_delete_contact( $contact_id ) {
 	}
 
 	/**
-	 * Fires before an item is deleted.
+	 * Fires before a contact is deleted.
 	 *
 	 * @param int $contact_id Contact id.
-	 * @param Contact $contact Contact object.
+	 * @param Contact $contact contact object.
 	 *
 	 * @since 1.2.1
 	 *
@@ -293,7 +291,7 @@ function eaccounting_delete_contact( $contact_id ) {
 	wp_cache_set( 'last_changed', microtime(), 'ea_contacts' );
 
 	/**
-	 * Fires after an contact is deleted.
+	 * Fires after a contact is deleted.
 	 *
 	 * @param int $contact_id contact id.
 	 * @param Contact $contact contact object.
@@ -308,125 +306,12 @@ function eaccounting_delete_contact( $contact_id ) {
 }
 
 /**
- * Sanitizes every contact field.
- *
- * If the context is 'raw', then the contact object or array will get minimal
- * sanitization of the integer fields.
- *
- * @param object|array $contact The contact object or array
- * @param string $context Optional. How to sanitize post fields. Accepts 'raw', 'edit', 'db', 'display'. Default 'display'.
- *
- * @return object|Contact|array The now sanitized contact object or array
- * @see eaccounting_sanitize_contact_field()
- *
- * @since 1.2.1
- */
-function eaccounting_sanitize_contact( $contact, $context = 'raw' ) {
-	if ( is_object( $contact ) ) {
-		// Check if post already filtered for this context.
-		if ( isset( $contact->filter ) && $context == $contact->filter ) {
-			return $contact;
-		}
-		if ( ! isset( $contact->id ) ) {
-			$contact->id = 0;
-		}
-		foreach ( array_keys( get_object_vars( $contact ) ) as $field ) {
-			$contact->$field = eaccounting_sanitize_contact_field( $field, $contact->$field, $contact->id, $context );
-		}
-		$contact->filter = $context;
-	} elseif ( is_array( $contact ) ) {
-		// Check if post already filtered for this context.
-		if ( isset( $contact['filter'] ) && $context == $contact['filter'] ) { //phpcs:ignore
-			return $contact;
-		}
-		if ( ! isset( $contact['id'] ) ) {
-			$contact['id'] = 0;
-		}
-		foreach ( array_keys( $contact ) as $field ) {
-			$contact[ $field ] = eaccounting_sanitize_contact_field( $field, $contact[ $field ], $contact['id'], $context );
-		}
-		$contact['filter'] = $context;
-	}
-
-	return $contact;
-}
-
-/**
- * Sanitizes a contact field based on context.
- *
- * Possible context values are:  'raw', 'edit', 'db', 'display'.
- *
- * @param string $field The contact Object field name.
- * @param mixed $value The contact Object value.
- * @param int $contact_id contact id.
- * @param string $context Optional. How to sanitize the field. Possible values are 'raw', 'edit','db', 'display'. Default 'display'.
- *
- * @return mixed Sanitized value.
- * @since 1.2.1
- */
-function eaccounting_sanitize_contact_field( $field, $value, $contact_id, $context ) {
-	if ( false !== strpos( $field, '_id' ) || $field === 'id' ) { //phpcs:ignore
-		$value = absint( $value );
-	}
-
-	$context = strtolower( $context );
-
-	if ( 'raw' === $context ) {
-		return $value;
-	}
-
-	if ( 'edit' === $context ) {
-
-		/**
-		 * Filters an contact field to edit before it is sanitized.
-		 *
-		 * @param mixed $value Value of the contact field.
-		 * @param int $contact_id Contact id.
-		 *
-		 * @since 1.2.1
-		 */
-		$value = apply_filters( "eaccounting_edit_contact_{$field}", $value, $contact_id );
-
-	} elseif ( 'db' === $context ) {
-
-		/**
-		 * Filters a contact field value before it is sanitized.
-		 *
-		 * @param mixed $value Value of the contact field.
-		 * @param int $contact_id Contact id.
-		 *
-		 * @since 1.2.1
-		 */
-		$value = apply_filters( "eaccounting_pre_contact_{$field}", $value, $contact_id );
-
-	} else {
-		// Use display filters by default.
-
-		/**
-		 * Filters the contact field sanitized for display.
-		 *
-		 * The dynamic portion of the filter name, `$field`, refers to the contact field name.
-		 *
-		 * @param mixed $value Value of the item field.
-		 * @param int $contact_id contact id.
-		 * @param string $context Context to retrieve the contact field value.
-		 *
-		 * @since 1.2.1
-		 */
-		$value = apply_filters( "eaccounting_contact_{$field}", $value, $contact_id, $context );
-	}
-
-	return $value;
-}
-
-/**
  * Retrieves an array of the contacts matching the given criteria.
  *
  * @param array $args Arguments to retrieve contacts.
  *
  * @return Contact[]|int Array of contact objects or count.
  * @since 1.1.0
- *
  */
 function eaccounting_get_contacts( $args = array() ) {
 	$defaults = array(
@@ -446,6 +331,119 @@ function eaccounting_get_contacts( $args = array() ) {
 		return $query->get_total();
 	}
 
-
 	return $query->get_results();
+}
+
+/**
+ * Sanitizes every contact field.
+ *
+ * If the context is 'raw', then the contact object or array will get minimal
+ * sanitization of the integer fields.
+ *
+ * @param object|array $contact The contact object or array
+ * @param string       $context Optional. How to sanitize post fields. Accepts 'raw', 'edit', 'db', 'display'. Default 'display'.
+ *
+ * @return object|Contact|array The now sanitized contact object or array
+ * @see eaccounting_sanitize_contact_field()
+ *
+ * @since 1.2.1
+ */
+function eaccounting_sanitize_contact( $contact, $context = 'raw' ) {
+	if ( is_object( $contact ) ) {
+		// Check if post already filtered for this context.
+		if ( isset( $contact->filter ) && $context === $contact->filter ) {
+			return $contact;
+		}
+		if ( ! isset( $contact->id ) ) {
+			$contact->id = 0;
+		}
+
+		foreach ( array_keys( get_object_vars( $contact ) ) as $field ) {
+			$contact->$field = eaccounting_sanitize_contact_field( $field, $contact->$field, $contact->id, $context );
+		}
+		$contact->filter = $context;
+	} elseif ( is_array( $contact ) ) {
+		// Check if post already filtered for this context.
+		if ( isset( $contact['filter'] ) && $context === $contact['filter'] ) {
+			return $contact;
+		}
+		if ( ! isset( $contact['id'] ) ) {
+			$contact['id'] = 0;
+		}
+		foreach ( array_keys( $contact ) as $field ) {
+			$contact[ $field ] = eaccounting_sanitize_contact_field( $field, $contact[ $field ], $contact['id'], $context );
+		}
+		$contact['filter'] = $context;
+	}
+
+	return $contact;
+}
+
+/**
+ * Sanitizes contact field based on context.
+ *
+ * Possible context values are:  'raw', 'edit', 'db', 'display'.
+ *
+ * @param string $field The contact Object field name.
+ * @param mixed  $value The contact Object value.
+ * @param int    $contact_id contact id.
+ * @param string $context Optional. How to sanitize the field. Possible values are 'raw', 'edit','db', 'display'. Default 'display'.
+ *
+ * @return mixed Sanitized value.
+ * @since 1.2.1
+ */
+function eaccounting_sanitize_contact_field( $field, $value, $contact_id, $context ) {
+	if ( false !== strpos( $field, '_id' ) || $field === 'id' ) {// phpcs:ignore
+		$value = absint( $value );
+	}
+
+	$context = strtolower( $context );
+
+	if ( 'raw' === $context ) {
+		if ( $field === 'extra' ) {// phpcs:ignore
+			$value = maybe_unserialize( $value );
+		}
+
+		return $value;
+	}
+
+	if ( 'edit' === $context ) {
+
+		/**
+		 * Filters contact field to edit before it is sanitized.
+		 *
+		 * @param mixed $value Value of the contact field.
+		 * @param int $contact_id Contact id.
+		 *
+		 * @since 1.2.1
+		 */
+		$value = apply_filters( "eaccounting_edit_contact_{$field}", $value, $contact_id );
+
+	} elseif ( 'db' === $context ) {
+
+		/**
+		 * Filters contact field value before it is sanitized.
+		 *
+		 * @param mixed $value Value of the contact field.
+		 * @param int $contact_id Contact id.
+		 *
+		 * @since 1.2.1
+		 */
+		$value = apply_filters( "eaccounting_pre_contact_{$field}", $value, $contact_id );
+	} else {
+		// Use display filters by default.
+
+		/**
+		 * Filters the contact field sanitized for display.
+		 *
+		 * @param mixed $value Value of the contact field.
+		 * @param int $contact_id Contact id.
+		 * @param string $context Context to retrieve the account field value.
+		 *
+		 * @since 1.2.1
+		 */
+		$value = apply_filters( "eaccounting_contact_{$field}", $value, $contact_id, $context );
+	}
+
+	return $value;
 }
