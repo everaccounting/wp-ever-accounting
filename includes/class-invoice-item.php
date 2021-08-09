329e5d2ea749ae02c5ed5a8dc96719f92a924798
Sultan Nasir Uddin <manikdrmc@gmail.com>
2021-08-09 10:25:25 +0600

    update invoice item

--- includes/class-invoice-item.php
+++ includes/class-invoice-item.php
@@ -34,182 +34,56 @@ defined( 'ABSPATH' ) || exit;
  * @property string $date_created
  */
 class Invoice_Item extends Data {
-	/**
-	 * Invoice Item id.
-	 *
-	 * @since 1.2.1
-	 * @var int
-	 */
-	public $id = null;
 
 	/**
+	 * Item Data array.
 	 *
+	 * @since 1.1.0
 	 *
-	 * @since 1.2.1
-	 * @var null
+	 * @var array
 	 */
-	public $document_id = null;
+	public $data = array(
+		'invoice_id'    => null,
+		'item_id'       => null,
+		'item_name'     => '',
+		'price'         => 0.00,
+		'quantity'      => 1,
+		'subtotal'      => 0.00,
+		'tax_rate'      => 0.00,
+		'discount'      => 0.00,
+		'tax'           => 0.00,
+		'total'         => 0.00,
+		'currency_code' => '',
+		'extra'         => array(
+			'shipping'     => 0.00,
+			'shipping_tax' => 0.00,
+			'fees'         => 0.00,
+			'fees_tax'     => 0.00,
+		),
+		'date_created'  => null,
+	);
 
 	/**
+	 * A map of database fields to data types.
 	 *
+	 * @since 1.1.0
 	 *
-	 * @since 1.2.1
-	 * @var null
+	 * @var array
 	 */
-	public $item_id = null;
-
-	/**
-	 *
-	 *
-	 * @since 1.2.1
-	 * @var string
-	 */
-	public $item_name = '';
-
-	/**
-	 *
-	 *
-	 * @since 1.2.1
-	 * @var float
-	 */
-	public $price = 0.00;
-
-	/**
-	 *
-	 *
-	 * @since 1.2.1
-	 * @var int
-	 */
-	public $quantity = 1;
-
-	/**
-	 *
-	 *
-	 * @since 1.2.1
-	 * @var float
-	 */
-	public $subtotal = 0.00;
-
-	/**
-	 *
-	 *
-	 * @since 1.2.1
-	 * @var int
-	 */
-	public $tax_rate = 0;
-
-	/**
-	 *
-	 *
-	 * @since 1.2.1
-	 * @var float
-	 */
-	public $discount = 0.00;
-
-	/**
-	 *
-	 *
-	 * @since 1.2.1
-	 * @var float
-	 */
-	public $tax = 0.00;
-
-	/**
-	 *
-	 *
-	 * @since 1.2.1
-	 * @var float
-	 */
-	public $total = 0.00;
-
-	/**
-	 *
-	 *
-	 * @since 1.2.1
-	 * @var string
-	 */
-	public $currency_code = '';
-
-	/**
-	 *
-	 *
-	 * @since 1.2.1
-	 * @var string
-	 */
-	public $extra = '';
-
-	/**
-	 * Invoice item date.
-	 *
-	 * @since 1.2.1
-	 * @var string
-	 */
-	public $date_created = '0000-00-00 00:00:00';
-
-	/**
-	 * Retrieve Item instance.
-	 *
-	 * @param int $item_id Item id.
-	 *
-	 * @return Invoice_Item|false Item object, false otherwise.
-	 * @since 1.2.1
-	 *
-	 * @global \wpdb $wpdb WordPress database abstraction object.
-	 *
-	 */
-	public static function get_instance( $item_id ) {
-		global $wpdb;
-
-		$item_id = (int) $item_id;
-		if ( ! $item_id ) {
-			return false;
-		}
-
-		$_item = wp_cache_get( $item_id, 'ea_invoice_items' );
-
-		if ( ! $_item ) {
-			$_item = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}ea_invoice_items WHERE id = %d LIMIT 1", $item_id ) );
-
-			if ( ! $_item ) {
-				return false;
-			}
-			$_item = eaccounting_sanitize_invoice_item( $_item, 'raw' );
-			wp_cache_add( $_item->id, $_item, 'ea_invoice_items' );
-		}
-
-		return new self( $_item );
-	}
-
-	/**
-	 * Item constructor.
-	 *
-	 * @param $item
-	 *
-	 * @since 1.2.1
-	 */
-	public function __construct( $item = null ) {
-		foreach ( get_object_vars( $item ) as $key => $value ) {
-			$this->$key = $value;
-		}
-	}
-
-	/**
-	 * Filter item object based on context.
-	 *
-	 * @param string $context Context.
-	 *
-	 * @return Invoice_Item|Object
-	 * @since 1.2.1
-	 */
-	public function filter( $context ) {
-		if ( $this->context === $context ) {
-			return $this;
-		}
-
-		if ( 'raw' === $context ) {
-			return self::get_instance( $this->id );
-		}
-
-		return eaccounting_sanitize_invoice_item( $this, $context );
-	}
+	protected $data_type = array(
+		'id'            => '%d',
+		'document_id'   => '%d',
+		'item_id'       => '%d',
+		'item_name'     => '%s',
+		'price'         => '%.4f',
+		'quantity'      => '%.2f',
+		'subtotal'      => '%.4f',
+		'tax_rate'      => '%.4f',
+		'discount'      => '%.4f',
+		'tax'           => '%.4f',
+		'total'         => '%.4f',
+		'currency_code' => '%s',
+		'extra'         => '%s',
+		'date_created'  => '%s',
+	);
 }
