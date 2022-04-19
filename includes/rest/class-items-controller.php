2e63ea54fe0d25b8b5f7741c83856cd6cae06137
Sudipto Shakhari <Sudipto004.ruet@gmail.com>
2022-04-19 11:04:31 +0600

    update items rest controller and checked

--- includes/rest/class-items-controller.php
+++ includes/rest/class-items-controller.php
@@ -9,12 +9,12 @@
 
 namespace Ever_Accounting\REST;
 
-use Ever_Accounting\Abstracts\Entities_Controller;
-use Ever_Accounting\Models\Item;
+use Ever_Accounting\Item;
+use Ever_Accounting\Items;
 
 defined( 'ABSPATH' ) || die();
 
-class Items_Controller extends Entities_Controller {
+class Items_Controller extends REST_Controller {
 	/**
 	 * Route base.
 	 *
@@ -23,35 +23,486 @@ class Items_Controller extends Entities_Controller {
 	protected $rest_base = 'items';
 
 	/**
-	 * Entity type.
+	 * Registers the routes for the objects of the controller.
 	 *
-	 * @since 1.1.1
+	 * @since 1.1.0
 	 *
-	 * @var string
+	 * @see register_rest_route()
 	 */
-	protected $entity_type = "item";
+	public function register_routes() {
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base,
+			array(
+				array(
+					'methods'             => \WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_items' ),
+					'permission_callback' => array( $this, 'get_items_permissions_check' ),
+					'args'                => $this->get_collection_params(),
+				),
+				array(
+					'methods'             => \WP_REST_Server::CREATABLE,
+					'callback'            => array( $this, 'create_item' ),
+					'permission_callback' => array( $this, 'create_item_permissions_check' ),
+					'args'                => $this->get_endpoint_args_for_item_schema( \WP_REST_Server::CREATABLE ),
+				),
+				'schema' => array( $this, 'get_public_item_schema' ),
+			)
+		);
+
+		$get_item_args = array(
+			'context' => $this->get_context_param( array( 'default' => 'view' ) ),
+		);
+
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base . '/(?P<id>[\d]+)',
+			array(
+				'args'   => array(
+					'id' => array(
+						'description' => __( 'Unique identifier for the item.', 'wp-ever-accounting' ),
+					),
+				),
+				array(
+					'methods'             => \WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_item' ),
+					'permission_callback' => array( $this, 'get_item_permissions_check' ),
+					'args'                => $get_item_args,
+				),
+				array(
+					'methods'             => \WP_REST_Server::EDITABLE,
+					'callback'            => array( $this, 'update_item' ),
+					'permission_callback' => array( $this, 'update_item_permissions_check' ),
+					'args'                => $this->get_endpoint_args_for_item_schema( \WP_REST_Server::EDITABLE ),
+				),
+				array(
+					'methods'             => \WP_REST_Server::DELETABLE,
+					'callback'            => array( $this, 'delete_item' ),
+					'permission_callback' => array( $this, 'delete_item_permissions_check' ),
+				),
+				'schema' => array( $this, 'get_public_item_schema' ),
+			)
+		);
+	}
 
 	/**
-	 * Entity model class.
+	 * Checks if a given request has access to read items.
 	 *
-	 * @since 1.1.1
-	 *
-	 * @var string
-	 */
-	protected $entity_model = Item::class;
-
-	/**
-	 * Get objects.
-	 *
-	 * @param array $query_args Query args.
 	 * @param \WP_REST_Request $request Full details about the request.
 	 *
-	 * @return array|int|\WP_Error
-	 * @since  1.1.1
-	 *
+	 * @since 1.2.1
+	 * @return true|\WP_Error True if the request has read access, WP_Error object otherwise.
 	 */
-	protected function get_objects( $query_args, $request ) {
-		return eaccounting_get_items( $query_args );
+	public function get_items_permissions_check( $request ) {
+		if ( ! current_user_can( 'ea_manage_item' ) ) {
+			return new \WP_Error(
+				'rest_forbidden_context',
+				__( 'Sorry, you are not allowed to view items.', 'wp-ever-accounting' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Retrieves a collection of items.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 *
+	 * @since 1.2.1
+	 * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+	 */
+	public function get_items( $request ) {
+		// Ensure an include parameter is set in case the orderby is set to 'include'.
+		if ( ! empty( $request['orderby'] ) && 'include' === $request['orderby'] && empty( $request['include'] ) ) {
+			return new \WP_Error(
+				'rest_orderby_include_missing_include',
+				__( 'You need to define an include parameter to order by include.', 'wp-ever-accounting' ),
+				array( 'status' => 400 )
+			);
+		}
+
+		// Filter the query arguments for a request.
+		$args = apply_filters( 'ever_accounting_rest_item_query', $request->get_params(), $request );
+
+		$products = Items::query( $args );
+		$total      = Items::query( $args, true );
+
+		$items = array();
+		foreach ( $products as $product ) {
+			$data    = $this->prepare_item_for_response( $product, $request );
+			$items[] = $this->prepare_response_for_collection( $data );
+		}
+
+		$page      = (int) $args['paged'];
+		$max_pages = ceil( $total / (int) $args['per_page'] );
+
+		if ( $page > $max_pages && $total > 0 ) {
+			return new \WP_Error(
+				'rest_item_invalid_page_number',
+				__( 'The page number requested is larger than the number of pages available.', 'wp-ever-accounting' ),
+				array( 'status' => 400 )
+			);
+		}
+
+		$response = rest_ensure_response( $items );
+
+		$response->header( 'X-WP-Total', (int) $total );
+		$response->header( 'X-WP-TotalPages', (int) $max_pages );
+
+		$request_params = $request->get_query_params();
+		$base           = add_query_arg( urlencode_deep( $request_params ), rest_url( sprintf( '%s/%s', $this->namespace, $this->rest_base ) ) );
+
+		if ( $page > 1 ) {
+			$prev_page = $page - 1;
+
+			if ( $prev_page > $max_pages ) {
+				$prev_page = $max_pages;
+			}
+
+			$prev_link = add_query_arg( 'page', $prev_page, $base );
+			$response->link_header( 'prev', $prev_link );
+		}
+		if ( $max_pages > $page ) {
+			$next_page = $page + 1;
+			$next_link = add_query_arg( 'page', $next_page, $base );
+
+			$response->link_header( 'next', $next_link );
+		}
+
+		return $response;
+	}
+
+
+	/**
+	 * Checks if a given request has access to read an item.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 *
+	 * @since 1.2.1
+	 * @return true|\WP_Error True if the request has read access for the item, WP_Error object otherwise.
+	 */
+	public function get_item_permissions_check( $request ) {
+		$item = Items::get( $request['id'] );
+		if ( empty( $item ) ) {
+			return new \WP_Error(
+				'rest_item_invalid_id',
+				__( 'Invalid item id..', 'wp-ever-accounting' ),
+				array( 'status' => 404 )
+			);
+		}
+
+		if ( ! current_user_can( 'ea_manage_item' ) ) {
+			return new \WP_Error(
+				'rest_forbidden_context',
+				__( 'Sorry, you are not allowed to get this item.', 'wp-ever-accounting' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Retrieves a single item.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 *
+	 * @since 1.2.1
+	 * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+	 */
+	public function get_item( $request ) {
+		$item = Items::get( $request['id'] );
+		if ( empty( $item ) ) {
+			return new \WP_Error(
+				'rest_item_invalid_id',
+				__( 'Invalid item id.', 'wp-ever-accounting' ),
+				array( 'status' => 404 )
+			);
+		}
+
+		$data = $this->prepare_item_for_response( $item, $request );
+
+		return rest_ensure_response( $data );
+	}
+
+	/**
+	 * Checks if a given request has access to create an item.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 *
+	 * @since 1.2.1
+	 * @return true|\WP_Error True if the request has access to create items, WP_Error object otherwise.
+	 */
+	public function create_item_permissions_check( $request ) {
+		if ( ! empty( $request['id'] ) ) {
+			return new \WP_Error(
+				'rest_item_exists',
+				__( 'Cannot create existing item.', 'wp-ever-accounting' ),
+				array( 'status' => 400 )
+			);
+		}
+
+		if ( ! current_user_can( 'ea_manage_item' ) ) {
+			return new \WP_Error(
+				'rest_cannot_create',
+				__( 'Sorry, you are not allowed to edit items.', 'wp-ever-accounting' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Creates a single item.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 *
+	 * @since 1.2.1
+	 * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+	 */
+	public function create_item( $request ) {
+		if ( ! empty( $request['id'] ) ) {
+			return new \WP_Error(
+				'rest_item_exists',
+				__( 'Cannot create existing item.', 'wp-ever-accounting' ),
+				array( 'status' => 400 )
+			);
+		}
+
+		$prepared_item = $this->prepare_item_for_database( $request );
+
+		if ( is_wp_error( $prepared_item ) ) {
+			return $prepared_item;
+		}
+
+		$item = Items::insert( wp_slash( (array) $prepared_item ) );
+
+		if ( is_wp_error( $item ) ) {
+
+			if ( 'db_insert_error' === $item->get_error_code() ) {
+				$item->add_data( array( 'status' => 500 ) );
+			} else {
+				$item->add_data( array( 'status' => 400 ) );
+			}
+
+			return $item;
+		}
+
+		$response = $this->prepare_item_for_response( $item, $request );
+		$response = rest_ensure_response( $response );
+
+		$response->set_status( 201 );
+		$response->header( 'Location', rest_url( sprintf( '%s/%s/%d', $this->namespace, $this->rest_base, $item->get_id() ) ) );
+
+		return $response;
+	}
+
+
+	/**
+	 * Checks if a given request has access to update a item.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 *
+	 * @since 1.2.1
+	 * @return true|\WP_Error True if the request has access to update the item, WP_Error object otherwise.
+	 */
+	public function update_item_permissions_check( $request ) {
+		$item = Items::get( $request['id'] );
+		if ( empty( $item ) ) {
+			return new \WP_Error(
+				'rest_item_invalid_id',
+				__( 'Invalid item id..', 'wp-ever-accounting' ),
+				array( 'status' => 404 )
+			);
+		}
+
+		if ( ! current_user_can( 'ea_manage_item' ) ) {
+			return new \WP_Error(
+				'rest_cannot_edit',
+				__( 'Sorry, you are not allowed to edit this item.', 'wp-ever-accounting' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Updates a single item.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 *
+	 * @since 1.2.1
+	 * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+	 */
+	public function update_item( $request ) {
+		$item = Items::get( $request['id'] );
+		$props    = $this->prepare_item_for_database( $request );
+
+		if ( is_wp_error( $props ) ) {
+			return $props;
+		}
+		$item->set_props( $props );
+		$is_error = $item->save();
+
+		if ( is_wp_error( $is_error ) ) {
+			if ( 'db_update_error' === $is_error->get_error_code() ) {
+				$is_error->add_data( array( 'status' => 500 ) );
+			} else {
+				$is_error->add_data( array( 'status' => 400 ) );
+			}
+
+			return $is_error;
+		}
+
+		$response = $this->prepare_item_for_response( $item, $request );
+
+		return rest_ensure_response( $response );
+	}
+
+	/**
+	 * Checks if a given request has access to delete an item.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 *
+	 * @since 1.2.1
+	 * @return true|\WP_Error True if the request has access to delete the item, WP_Error object otherwise.
+	 */
+	public function delete_item_permissions_check( $request ) {
+		$item = Items::get( $request['id'] );
+		if ( empty( $item ) ) {
+			return new \WP_Error(
+				'rest_item_invalid_id',
+				__( 'Invalid item id..', 'wp-ever-accounting' ),
+				array( 'status' => 404 )
+			);
+		}
+
+		if ( ! current_user_can( 'ea_manage_item' ) ) {
+			return new \WP_Error(
+				'rest_cannot_delete',
+				__( 'Sorry, you are not allowed to delete item.', 'wp-ever-accounting' ),
+				array( 'status' => rest_authorization_required_code() )
+			);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Deletes a single item.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 *
+	 * @since 1.2.1
+	 * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+	 */
+	public function delete_item( $request ) {
+		$item = Items::get( $request['id'] );
+		$request->set_param( 'context', 'edit' );
+		$data = $this->prepare_item_for_response( $item, $request );
+
+		if ( ! $item->delete() ) {
+			return new \WP_Error( 'rest_cannot_delete', __( 'The item cannot be deleted.', 'wp-ever-accounting' ), array( 'status' => 500 ) );
+		}
+
+		$response = new \WP_REST_Response();
+		$response->set_data(
+			array(
+				'deleted'  => true,
+				'previous' => $this->prepare_response_for_collection( $data ),
+			)
+		);
+
+		return $response;
+	}
+
+	/**
+	 * Prepares a single item output for response.
+	 *
+	 * @param Item $item Item object.
+	 * @param \WP_REST_Request $request Request object.
+	 *
+	 * @since 1.2.1
+	 * @return \WP_REST_Response Response object.
+	 */
+	public function prepare_item_for_response( $item, $request ) {
+		$data        = $item->get_data();
+		$format_date = array( 'date_created' );
+		// Format date values.
+		foreach ( $format_date as $key ) {
+			$data[ $key ] = $this->prepare_date_response( $data[ $key ] );
+		}
+
+		$context  = ! empty( $request['context'] ) ? $request['context'] : 'view';
+		$data     = $this->add_additional_fields_to_object( $data, $request );
+		$data     = $this->filter_response_by_context( $data, $context );
+		$response = rest_ensure_response( $data );
+		$response->add_links( $this->prepare_links( $item, $request ) );
+
+		/**
+		 * Filter item data returned from the REST API.
+		 *
+		 * @param \WP_REST_Response $response The response object.
+		 * @param Item $item Item object used to create response.
+		 * @param \WP_REST_Request $request Request object.
+		 */
+		return apply_filters( 'ever_accounting_rest_prepare_item', $response, $item, $request );
+	}
+
+	/**
+	 * Prepares a single item for create or update.
+	 *
+	 * @param \WP_REST_Request $request Request object.
+	 *
+	 * @since 1.2.1
+	 * @return array|\WP_Error item object or WP_Error.
+	 */
+	protected function prepare_item_for_database( $request ) {
+		$schema    = $this->get_item_schema();
+		$data_keys = array_keys( array_filter( $schema['properties'], array( $this, 'filter_writable_props' ) ) );
+		$props     = [];
+		// Handle all writable props.
+		foreach ( $data_keys as $key ) {
+			$value = $request[ $key ];
+			if ( ! is_null( $value ) ) {
+				switch ( $key ) {
+					default:
+						$props[ $key ] = $value;
+						break;
+				}
+			}
+		}
+
+		/**
+		 * Filters item before it is inserted via the REST API.
+		 *
+		 * @param array $props Item props.
+		 * @param \WP_REST_Request $request Request object.
+		 */
+		return apply_filters( 'ever_accounting_rest_pre_insert_item', $props, $request );
+	}
+
+	/**
+	 * Prepare links for the request.
+	 *
+	 * @param Item $item Object data.
+	 * @param \WP_REST_Request $request Request item.
+	 *
+	 * @return array Links for the given item.
+	 */
+	protected function prepare_links( $item, $request ) {
+		return array(
+			'self'       => array(
+				'href' => rest_url( sprintf( '/%s/%s/%d', $this->namespace, $this->rest_base, $item->get_id() ) ),
+			),
+			'collection' => array(
+				'href' => rest_url( sprintf( '/%s/%s', $this->namespace, $this->rest_base ) ),
+			),
+		);
 	}
 
 	/**
