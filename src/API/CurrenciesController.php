7cb807206e6ea16d98b937d7921ca4f533349119
relevenx <bappi.cs@gmail.com>
2023-06-14 11:50:36 +0600

    Update CurrenciesController.php

--- src/API/CurrenciesController.php
+++ src/API/CurrenciesController.php
@@ -127,3 +127,343 @@ class CurrenciesController extends Controller {
 		return true;
 	}
 }
+
+/**
+ * Retrieves a list of currencies.
+ *
+ * @param \WP_REST_Request $request Full details about the request.
+ *
+ * @since 1.2.1
+ * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+ */
+public function get_items( $request ) {
+	$params = $this->get_collection_params();
+	foreach ( $params as $key => $value ) {
+		if ( isset( $request[ $key ] ) ) {
+			$args[ $key ] = $request[ $key ];
+		}
+	}
+
+	/**
+	 * Filters the query arguments for a request.
+	 *
+	 * Enables adding extra arguments or setting defaults for a currency request.
+	 *
+	 * @param array            $args Key value array of query var to query value.
+	 * @param \WP_REST_Request $request The request used.
+	 *
+	 * @since 1.2.1
+	 */
+	$args = apply_filters( 'ever_accounting_rest_currency_query', $args, $request );
+
+	$currencies = eac_get_currencies( $args );
+	$total      = eac_get_currencies( $args, true );
+	$page       = isset( $request['page'] ) ? absint( $request['page'] ) : 1;
+	$max_pages  = ceil( $total / (int) $args['number'] );
+
+	// If requesting page is greater than max pages, return empty array.
+	if ( $page > $max_pages ) {
+		return new \WP_Error(
+			'rest_account_invalid_page_number',
+			__( 'The page number requested is larger than the number of pages available.', 'wp-ever-accounting' ),
+			array( 'status' => 400 )
+		);
+	}
+
+	$results = array();
+	foreach ( $currencies as $currency ) {
+		$data      = $this->prepare_item_for_response( $currency, $request );
+		$results[] = $this->prepare_response_for_collection( $data );
+	}
+
+	$response = rest_ensure_response( $results );
+
+	$response->header( 'X-WP-Total', (int) $total );
+	$response->header( 'X-WP-TotalPages', (int) $max_pages );
+
+	$request_params = $request->get_query_params();
+	$base           = add_query_arg( urlencode_deep( $request_params ), rest_url( sprintf( '%s/%s', $this->namespace, $this->rest_base ) ) );
+
+	if ( $page > 1 ) {
+		$prev_page = $page - 1;
+
+		if ( $prev_page > $max_pages ) {
+			$prev_page = $max_pages;
+		}
+
+		$prev_link = add_query_arg( 'page', $prev_page, $base );
+		$response->link_header( 'prev', $prev_link );
+	}
+	if ( $max_pages > $page ) {
+		$next_page = $page + 1;
+		$next_link = add_query_arg( 'page', $next_page, $base );
+
+		$response->link_header( 'next', $next_link );
+	}
+
+	return $response;
+}
+
+/**
+ * Retrieves a single currency.
+ *
+ * @param \WP_REST_Request $request Full details about the request.
+ *
+ * @since 1.2.1
+ * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+ */
+public function get_item( $request ) {
+	$currency = eac_get_currency( $request['id'] );
+	$data     = $this->prepare_item_for_response( $currency, $request );
+
+	return rest_ensure_response( $data );
+}
+
+/**
+ * Creates a single currency.
+ *
+ * @param \WP_REST_Request $request Full details about the request.
+ *
+ * @since 1.2.1
+ * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+ */
+public function create_item( $request ) {
+	if ( ! empty( $request['id'] ) ) {
+		return new \WP_Error(
+			'rest_account_exists',
+			__( 'Cannot create existing currency.', 'wp-ever-accounting' ),
+			array( 'status' => 400 )
+		);
+	}
+
+	$data = $this->prepare_item_for_database( $request );
+	if ( is_wp_error( $data ) ) {
+		return $data;
+	}
+
+	$currency = eac_insert_currency( $data );
+	if ( is_wp_error( $currency ) ) {
+		return $currency;
+	}
+
+	$response = $this->prepare_item_for_response( $currency, $request );
+	$response = rest_ensure_response( $response );
+
+	$response->set_status( 201 );
+	$response->header( 'Location', rest_url( sprintf( '%s/%s/%d', $this->namespace, $this->rest_base, $currency->get_id() ) ) );
+
+	return $response;
+
+}
+
+/**
+ * Updates a single currency.
+ *
+ * @param \WP_REST_Request $request Full details about the request.
+ *
+ * @since 1.2.1
+ * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+ */
+public function update_item( $request ) {
+	$currency = eac_get_currency( $request['id'] );
+	$data     = $this->prepare_item_for_database( $request );
+	if ( is_wp_error( $data ) ) {
+		return $data;
+	}
+
+	$currency = eac_insert_currency( $currency->get_id(), $data );
+	if ( is_wp_error( $currency ) ) {
+		return $currency;
+	}
+
+	$response = $this->prepare_item_for_response( $currency, $request );
+	$response = rest_ensure_response( $response );
+
+	return $response;
+}
+
+/**
+ * Deletes a single currency.
+ *
+ * @param \WP_REST_Request $request Full details about the request.
+ *
+ * @since 1.2.1
+ * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+ */
+public function delete_item( $request ) {
+	$currency = eac_get_currency( $request['id'] );
+	$request->set_param( 'context', 'edit' );
+	$data = $this->prepare_item_for_response( $currency, $request );
+
+	if ( ! eac_delete_currency( $currency->get_id() ) ) {
+		return new \WP_Error(
+			'rest_account_cannot_delete',
+			__( 'The currency cannot be deleted.', 'wp-ever-accounting' ),
+			array( 'status' => 500 )
+		);
+	}
+
+	$response = new \WP_REST_Response();
+	$response->set_data(
+		array(
+			'deleted'  => true,
+			'previous' => $this->prepare_response_for_collection( $data ),
+		)
+	);
+
+	return $response;
+}
+
+/**
+ * Prepares a single currency output for response.
+ *
+ * @param currency         $currency currency object.
+ * @param \WP_REST_Request $request Request object.
+ *
+ * @since 1.2.1
+ * @return \WP_REST_Response|\WP_Error Response object on success, or WP_Error object on failure.
+ */
+public function prepare_item_for_response( $currency, $request ) {
+	$data = [];
+
+	foreach ( array_keys( $this->get_schema_properties() ) as $key ) {
+		switch ( $key ) {
+			case 'created_at':
+			case 'updated_at':
+				$value = $this->prepare_date_response( $currency->$key );
+				break;
+			default:
+				$value = $currency->$key;
+				break;
+		}
+
+		$data[ $key ] = $value;
+	}
+
+	$context  = ! empty( $request['context'] ) ? $request['context'] : 'view';
+	$data     = $this->add_additional_fields_to_object( $data, $request );
+	$data     = $this->filter_response_by_context( $data, $context );
+	$response = rest_ensure_response( $data );
+	$response->add_links( $this->prepare_links( $currency, $request ) );
+
+	/**
+	 * Filter currency data returned from the REST API.
+	 *
+	 * @param \WP_REST_Response $response The response object.
+	 * @param currency          $currency currency object used to create response.
+	 * @param \WP_REST_Request  $request Request object.
+	 */
+	return apply_filters( 'ever_accounting_rest_prepare_currency', $response, $currency, $request );
+}
+
+/**
+ * Prepares a single currency for create or update.
+ *
+ * @param \WP_REST_Request $request Request object.
+ *
+ * @since 1.2.1
+ * @return array|\WP_Error currency object or WP_Error.
+ */
+protected function prepare_item_for_database( $request ) {
+	$schema    = $this->get_item_schema();
+	$data_keys = array_keys( array_filter( $schema['properties'], array( $this, 'filter_writable_props' ) ) );
+	$props     = [];
+	// Handle all writable props.
+	foreach ( $data_keys as $key ) {
+		$value = $request[ $key ];
+		if ( ! is_null( $value ) ) {
+			switch ( $key ) {
+				default:
+					$props[ $key ] = $value;
+					break;
+			}
+		}
+	}
+
+	/**
+	 * Filters currency before it is inserted via the REST API.
+	 *
+	 * @param array            $props currency props.
+	 * @param \WP_REST_Request $request Request object.
+	 */
+	return apply_filters( 'ever_accounting_rest_pre_insert_currency', $props, $request );
+}
+
+/**
+ * Prepare links for the request.
+ *
+ * @param currency         $currency Object data.
+ * @param \WP_REST_Request $request Request currency.
+ *
+ * @return array Links for the given currency.
+ */
+protected function prepare_links( $currency, $request ) {
+	return array(
+		'self'       => array(
+			'href' => rest_url( sprintf( '/%s/%s/%d', $this->namespace, $this->rest_base, $currency->get_id() ) ),
+		),
+		'collection' => array(
+			'href' => rest_url( sprintf( '/%s/%s', $this->namespace, $this->rest_base ) ),
+		),
+	);
+}
+/**
+ * Retrieves the item's schema, conforming to JSON Schema.
+ *
+ * @since 1.1.2
+ * @return array Item schema data.
+ */
+public function get_item_schema() {
+	$schema = array(
+		'$schema'    => 'http://json-schema.org/draft-04/schema#',
+		'title'      => __( 'currency', 'wp-ever-accounting' ),
+		'type'       => 'object',
+		'properties' => array(
+			'id'          => array(
+				'description' => __( 'Unique identifier for the currency.', 'wp-ever-accounting' ),
+				'type'        => 'integer',
+				'context'     => array( 'view', 'embed', 'edit' ),
+				'readonly'    => true,
+				'arg_options' => array(
+					'sanitize_callback' => 'intval',
+				),
+			),
+			'name'        => array(
+				'description' => __( 'currency name.', 'wp-ever-accounting' ),
+				'type'        => 'string',
+				'context'     => array( 'view', 'edit' ),
+				'required'    => true,
+			),
+			'description' => array(
+				'description' => __( 'currency description.', 'wp-ever-accounting' ),
+				'type'        => 'string',
+				'context'     => array( 'view', 'edit' ),
+			),
+			'updated_at'  => array(
+				'description' => __( "The date the currency was last updated, in the site's timezone.", 'wp-ever-accounting' ),
+				'type'        => 'date-time',
+				'context'     => array( 'view', 'edit' ),
+				'readonly'    => true,
+			),
+			'created_at'  => array(
+				'description' => __( "The date the currency was created, in the site's timezone.", 'wp-ever-accounting' ),
+				'type'        => 'date-time',
+				'context'     => array( 'view', 'edit' ),
+				'readonly'    => true,
+			),
+		),
+	);
+
+	/**
+	 * Filters the currency's schema.
+	 *
+	 * @param array $schema Item schema data.
+	 *
+	 * @since 1.2.1
+	 */
+	$schema = apply_filters( 'ever_accounting_rest_currency_item_schema', $schema );
+
+	return $this->add_additional_fields_schema( $schema );
+}
+
+}
