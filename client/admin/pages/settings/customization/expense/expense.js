d3acfc88c98c56b6bb3234aa173eb295220a3f6d
Mashrur Ayon <muhayon.rxz@gmail.com>
2023-11-20 10:50:11 +0600

    create expense customization page

---
+++ client/admin/pages/settings/customization/expense/expense.js
@@ -0,0 +1,21 @@
+/**
+ * External dependencies
+ */
+import { Card } from '@eac/components';
+import { navigate, getQuery } from '@eac/navigation';
+/**
+ * WordPress dependencies
+ */
+import { lazy } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+const Settings = lazy( () => import( './settings' ) );
+
+function Expense () {
+    return (
+            <Settings />
+    );
+}
+
+export default Expense;
+
