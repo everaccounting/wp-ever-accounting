f3787424ece3e5bedd4708f8916c2aba681d8218
Mashrur Ayon <muhayon.rxz@gmail.com>
2023-11-27 12:07:01 +0600

    create add expense component

--- packages/editor/src/components/index.js
+++ packages/editor/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as AddCategory } from './categories/add-category';
 export { default as AddItem } from './items/add-item';
 export { default as AddCustomer } from './customers/add-customer';
+export { default as AddExpense} from  './expenses/add-expense'
