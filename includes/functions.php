8bcd3f7b71a84aece690676a113a1423e30539ef
devsabbirhossain <sabbir2dev@gmail.com>
2024-10-29 13:21:30 +0600

    fix depricated issue

--- includes/functions.php
+++ includes/functions.php
@@ -122,12 +122,14 @@ function eac_format_amount( $amount, $currency = null ) {
  * @return float|int
  */
 function eac_sanitize_amount( $amount, $currency = null ) {
-	if ( ! is_numeric( $amount ) ) {
+	if ( ! is_numeric( $amount )  ) {
+		$amount     = sanitize_text_field($amount);
 		$currencies = eac_get_currencies();
 		$data       = array_key_exists( $currency, $currencies ) ? $currencies[ $currency ] : $currencies[ eac_base_currency() ];
 
 		// Remove currency symbol.
 		$amount = str_replace( $data['symbol'], '', $amount );
+
 		// Remove any non-numeric characters except a thousand and decimal separators.
 		$amount = preg_replace( '/[^0-9\\' . $data['thousand'] . '\\' . $data['decimal'] . '\-\+]/', '', $amount );
 		// Replace a thousand and decimal separators with empty string and dot respectively.
@@ -143,7 +145,7 @@ function eac_sanitize_amount( $amount, $currency = null ) {
 		}
 	}
 
-	return $amount;
+	return empty( $amount ) ? 0 : $amount;
 }
 
 /**
